0b975b46869660c7923a3af3664960b7871ba2bb
Tao Wen <taowen@gmail.com>
2012-07-31 10:52:41 +0800

    install resweb

--- src/veil/frontend/pyres/_script/installer.py
+++ src/veil/frontend/pyres/_script/installer.py
@@ -1,12 +1,15 @@
 from __future__ import unicode_literals, print_function, division
 from veil.frontend.script import *
 from veil.environment.deployment import *
+from veil.frontend.template import *
 
 
 @script('install')
 def install_pyres():
+    settings = get_deployment_settings()
     install_ubuntu_package('python2.7-dev')
     execute_script('backend', 'redis', 'client', 'install')
     execute_script('backend', 'redis', 'server', 'install', 'queue')
     install_python_package('pyres')
     install_python_package('croniter')
+    create_file(settings.resweb.config_file, content=get_template('resweb.cfg.j2').render(config=settings.resweb))
--- src/veil/frontend/pyres/setting.py
+++ src/veil/frontend/pyres/setting.py
@@ -8,10 +8,23 @@ PYRES_BASE_SETTINGS = {
         'configfile': VEIL_ETC_DIR / 'queue.conf',
         'port': 6380
     },
+    'resweb': {
+        'config_file': VEIL_ETC_DIR / 'resweb.cfg',
+        'queue_host': 'localhost',
+        'queue_port': 6380,
+        'server_host': 'localhost',
+        'server_port': 5000
+    },
     'supervisor': {
         'programs': {
             'queue': {
                 'command': 'veil backend redis server up queue'
+            },
+            'resweb': {
+                'command': 'resweb',
+                'environment_variables': {
+                    'RESWEB_SETTINGS': VEIL_ETC_DIR / 'resweb.cfg'
+                }
             }
         }
     }
