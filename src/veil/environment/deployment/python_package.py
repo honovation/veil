a910cebab863399bb421152d982e4a7bd0ce0162
Tao Wen <taowen@gmail.com>
2012-08-06 13:34:53 +0800

    support specify python mirror

--- src/veil/environment/deployment/python_package.py
+++ src/veil/environment/deployment/python_package.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals, print_function, division
+import os
 from veil.backend.shell import *
 
 def install_python_package(package_name, test_package=None):
@@ -6,4 +7,8 @@ def install_python_package(package_name, test_package=None):
     try:
         __import__(test_package)
     except ImportError:
-        shell_execute('pip install {}'.format(package_name))
+        mirror = os.getenv('VEIL_PYTHON_PACKAGE_MIRROR')
+        if mirror:
+            shell_execute('pip install {} --no-index -f {}'.format(package_name, mirror))
+        else:
+            shell_execute('pip install {}'.format(package_name))
