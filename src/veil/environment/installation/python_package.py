4724706483ee22538f854a25e79b7649a7cd0465
Tao Wen <taowen@gmail.com>
2012-08-20 15:19:05 +0800

    install db2 client

--- src/veil/environment/installation/python_package.py
+++ src/veil/environment/installation/python_package.py
@@ -2,13 +2,13 @@ from __future__ import unicode_literals, print_function, division
 import os
 from veil.backend.shell import *
 
-def install_python_package(package_name, test_package=None):
+def install_python_package(package_name, test_package=None, **kwargs):
     test_package = test_package or package_name
     try:
         __import__(test_package)
     except ImportError:
         mirror = os.getenv('VEIL_PYTHON_PACKAGE_MIRROR')
         if mirror:
-            shell_execute('pip install {} --no-index -f {}'.format(package_name, mirror))
+            shell_execute('pip install {} --no-index -f {}'.format(package_name, mirror), **kwargs)
         else:
-            shell_execute('pip install {}'.format(package_name))
+            shell_execute('pip install {}'.format(package_name), **kwargs)
