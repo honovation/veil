97755aeb2c5945eb65d117efb170d3d0927a1c52
Tao Wen <taowen@gmail.com>
2012-10-17 00:54:35 +0800

    static file no longer depends on lxml

--- src/veil/frontend/web/static_file/static_file_installer.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals, print_function, division
-from veil.environment.installation import *
-
-
-@installation_script()
-def install_web_server():
-    install_ubuntu_package('libxml2-dev')
-    install_ubuntu_package('libxslt1-dev')
-    install_python_package('lxml')
