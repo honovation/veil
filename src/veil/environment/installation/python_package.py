84768906eedc5c94689bffe5d25a8f99d98adfb1
Tao Wen <taowen@gmail.com>
2012-10-18 12:23:28 +0800

    default to use dependency-veil

--- src/veil/environment/installation/python_package.py
+++ src/veil/environment/installation/python_package.py
@@ -7,7 +7,7 @@ def install_python_package(package_name, test_package=None, **kwargs):
     try:
         __import__(test_package)
     except ImportError:
-        mirror = os.getenv('VEIL_PYTHON_PACKAGE_MIRROR')
+        mirror = os.getenv('VEIL_PYTHON_PACKAGE_MIRROR', 'http://dependency-veil.googlecode.com/svn/trunk/')
         if mirror:
             shell_execute('pip install {} --no-index -f {}'.format(package_name, mirror), **kwargs)
         else:
