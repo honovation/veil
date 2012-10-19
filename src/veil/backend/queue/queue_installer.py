b0398bffd49aa951d97d910a10b696ad6890c4ab
Tao Wen <taowen@gmail.com>
2012-10-19 21:29:01 +0800

    launcher is mixed with queue api, so we need to install crontier and pytz as well

--- src/veil/backend/queue/queue_installer.py
+++ src/veil/backend/queue/queue_installer.py
@@ -14,6 +14,7 @@ def install_queue_api():
 @script('install-resweb')
 def install_queue_server():
     settings = get_settings()
+    install_queue_api()
     install_python_package('resweb')
     create_file(settings.resweb.config_file, content=get_template('resweb.cfg.j2').render(
         queue_config=settings.queue_redis,
@@ -22,15 +23,15 @@ def install_queue_server():
 
 @script('install-delayed-job-scheduler')
 def install_delayed_job_scheduler():
-    install_python_package('pyres')
+    install_queue_api()
 
 
 @script('install-periodic-job-scheduler')
 def install_delayed_job_scheduler():
-    install_python_package('pyres')
+    install_queue_api()
 
 
 @script('install-worker')
 def install_worker(queue_name):
-    install_python_package('pyres')
+    install_queue_api()
     # TODO: install the components where job handlers resides in
