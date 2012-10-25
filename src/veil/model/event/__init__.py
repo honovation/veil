eb78751b3e09b3fe168f526f20c96131702e696c
Tao Wen <taowen@gmail.com>
2012-10-25 19:12:26 +0800

    add @event syntax sugar

--- src/veil/model/event/__init__.py
+++ src/veil/model/event/__init__.py
@@ -4,6 +4,7 @@ with veil.component.init_component(__name__):
     from .event import publish_event
     from .event import subscribe_event
     from .event import unsubscribe_event
+    from .event import event
     from .assertion import assert_event_published
     from .assertion import assert_event_not_published
 
@@ -12,6 +13,7 @@ with veil.component.init_component(__name__):
         publish_event.__name__,
         subscribe_event.__name__,
         unsubscribe_event.__name__,
+        event.__name__,
         # from assertation
         assert_event_published.__name__,
         assert_event_not_published.__name__
--- src/veil/model/event/event.py
+++ src/veil/model/event/event.py
@@ -17,11 +17,20 @@ def publish_event(topic, **kwargs):
                 topic, subscriber.__module__, subscriber, exc_class.__name__, exc))
             raise new_exc.__class__, new_exc, tb
 
+
 def subscribe_event(topic, subscriber):
     subscribers[topic] = subscribers.get(topic, [])
     subscribers[topic].append(subscriber)
+
 
 def unsubscribe_event(topic, subscriber):
     topic_subscribers = subscribers.get(topic, [])
     if subscriber in topic_subscribers:
         topic_subscribers.remove(subscriber)
+
+
+def event(topic): #syntax sugar
+    def decorator(subscriber):
+        subscribe_event(topic, subscriber)
+
+    return decorator
