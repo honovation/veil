14d6b9673fb54b9a4c00cd4fae10d955931f8b61
Tao Wen <taowen@gmail.com>
2012-10-25 21:41:57 +0800

    fix test env can not reset database bug

--- src/veil/supervisor/supervisorctl.py
+++ src/veil/supervisor/supervisorctl.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals, print_function, division
 from veil.environment.setting import *
 from veil.backend.shell import *
+from .supervisor_setting import supervisor_settings
 
 def supervisorctl(action, *arguments):
     shell_execute('veil execute supervisorctl -c {} {} {}'.format(
@@ -8,4 +9,5 @@ def supervisorctl(action, *arguments):
 
 
 def get_option(key):
-    return get_settings()['supervisor'][key]
+    settings = merge_settings(supervisor_settings(), get_settings(), overrides=True)
+    return settings['supervisor'][key]
