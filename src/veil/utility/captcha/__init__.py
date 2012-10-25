928386ea67f191aa86f9a563551ec91339ea3b14
Tao Wen <taowen@gmail.com>
2012-10-26 04:39:32 +0800

    fix captcha

--- src/veil/utility/captcha/__init__.py
+++ src/veil/utility/captcha/__init__.py
@@ -4,5 +4,5 @@ with veil.component.init_component(__name__):
     from .captcha import register_captcha
 
     __all__ = [
-        register_captcha_protection.__name__
+        register_captcha.__name__
     ]
