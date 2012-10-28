833d0959f8012d6d0dcd7923dfb96b5da2f994e8
Tao Wen <taowen@gmail.com>
2012-10-29 06:16:44 +0800

    add a inner dependency-veil server

--- src/veil/environment/installation/python_package.py
+++ src/veil/environment/installation/python_package.py
@@ -13,7 +13,7 @@ def install_python_package(package_name, test_package=None, **kwargs):
     except ImportError:
         LOGGER.info('installing python package {} ...'.format(package_name))
 #        mirror = os.getenv('VEIL_PYTHON_PACKAGE_MIRROR', 'http://dependency-veil.googlecode.com/svn/trunk/')
-        mirror = os.getenv('VEIL_PYTHON_PACKAGE_MIRROR', 'http://www.dmright.com/')
+        mirror = os.getenv('VEIL_PYTHON_PACKAGE_MIRROR', 'http://200.200.200.25:8080/')
         if mirror:
             shell_execute('pip install {} --no-index -f {}'.format(package_name, mirror), **kwargs)
         else:
