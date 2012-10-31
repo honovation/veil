7beced74152fcca75a5603650a80e8166a04f3af
Tao Wen <taowen@gmail.com>
2012-10-31 12:12:27 +0800

    veil pull before self-check

--- src/veil/development/self_checker/checker.py
+++ src/veil/development/self_checker/checker.py
@@ -12,11 +12,12 @@ def register_self_checker(name, self_checker):
 
 @script('self-check')
 def self_check(checker_name=None):
+    shell_execute('git add .')
+    shell_execute('veil pull')
     if checker_name:
         self_checkers[checker_name]()
         return
     for checker_name, self_checker in self_checkers.items():
         print('[CHECK] checking {}...'.format(checker_name))
         self_checker()
-    shell_execute('git add .')
     (VEIL_HOME / '.self-check-passed').write_text(discipline_coach.calculate_git_status_hash())
