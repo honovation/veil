48730e7b4bd0508368d3430eb6c24543ced9e7d6
Tao Wen <taowen@gmail.com>
2012-11-02 18:10:53 +0800

    support white_list in loc

--- src/veil/development/loc/loc_checker.py
+++ src/veil/development/loc/loc_checker.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals, print_function, division
 import os.path
 import veil_component
 
-WHITE_LIST = {'veil.utility.path'} # copied from somewhere
+WHITE_LIST = {'veil.utility.path', 'veil.frontend.web.tornado'} # copied from somewhere
 THRESHOLD = 700
 GOAL = 500
 
@@ -10,16 +10,17 @@ def check_loc():
     component_locs = {}
     sub_component_locs = {}
     for component_name in veil_component.get_loaded_components().keys():
-        if component_name not in WHITE_LIST:
-            component_locs[component_name] = calculate_component_loc(component_name)
+        component_locs[component_name] = calculate_component_loc(component_name)
     for component_name in sorted(component_locs.keys()):
         sub_component_locs[component_name] = calculate_sub_component_loc(component_locs, component_name)
+    max_loc = 0
     for component_name in sorted(component_locs.keys()):
         component_locs[component_name] -= sub_component_locs.get(component_name, 0)
         own_loc = component_locs[component_name]
-        if own_loc > THRESHOLD:
-            raise Exception('{} contains {} lines of code, extract component out!'.format(component_name, own_loc))
-    max_loc = max(component_locs.values())
+        if component_name not in WHITE_LIST:
+            max_loc = max(own_loc, max_loc)
+            if own_loc > THRESHOLD:
+                raise Exception('{} contains {} lines of code, extract component out!'.format(component_name, own_loc))
     if THRESHOLD > GOAL and THRESHOLD - max_loc > 50:
         raise Exception('Threshold can be reduced to {} now', max_loc)
 
@@ -44,7 +45,8 @@ def calculate_component_loc(component_name):
     loc = 0
     for module_name, module in veil_component.force_get_all_loaded_modules().items():
         if module_name.startswith(prefix):
-            loc += get_loc(module.__file__.replace('.pyc', '.py'))
+            if hasattr(module, '__file__'):
+                loc += get_loc(module.__file__.replace('.pyc', '.py'))
     return loc
 
 
