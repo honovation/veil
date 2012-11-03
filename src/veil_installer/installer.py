d182bb7737ebacd94094eada864c4b4927f8f1dc
Tao Wen <taowen@gmail.com>
2012-11-03 13:43:14 +0800

    support component resource installation

--- src/veil_installer/installer.py
+++ src/veil_installer/installer.py
@@ -12,6 +12,13 @@ def install_resource(is_dry_run, resource):
         pprint.pprint(list_resource_map(resource))
         return
 
+    installer_name, installer_args = resource
+    if installer_name not in INSTALLERS:
+        raise Exception('no installer for: {} {}'.format(installer_name, installer_args))
+    more_resources = INSTALLERS[installer_name](is_dry_run=False, **installer_args)
+    for more_resource in more_resources or []:
+        install_resource(False, more_resource)
+
 
 def list_resource_map(resource):
     installer_name, installer_args = resource
@@ -23,7 +30,11 @@ def list_resource_map(resource):
         return '-' if result else 'INSTALL'
     resource_map = {}
     for resource in result:
-        resource_map[format_resource(resource)] = list_resource_map(resource)
+        sub_resource_map = list_resource_map(resource)
+        if isinstance(sub_resource_map, dict):
+            resource_map.update(sub_resource_map)
+        else:
+            resource_map[format_resource(resource)] = list_resource_map(resource)
     return resource_map
 
 
