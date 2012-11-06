715c03072f0a654e21a08eced00b5b72bb54ebcf
Tao Wen <taowen@gmail.com>
2012-11-06 15:30:44 +0800

    can not ensure migrate run before correctness checker, need to find another way to reset test database before test execution

--- src/veil/environment/deployment/__init__.py
+++ src/veil/environment/deployment/__init__.py
@@ -6,8 +6,3 @@ with veil_component.init_component(__name__):
     __all__ = [
         register_migration_command.__name__
     ]
-    def init():
-        from veil.development.self_checker import register_self_checker
-        from .local_deployer import migrate
-
-        register_self_checker('ensure-migrate', migrate)
--- src/veil/environment/deployment/local_deployer.py
+++ src/veil/environment/deployment/local_deployer.py
@@ -1,7 +1,5 @@
 from __future__ import unicode_literals, print_function, division
-import sys
 from veil.frontend.cli import *
-from veil.environment.supervisor import *
 from veil.backend.shell import *
 
 migration_commands = set()
