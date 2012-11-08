f87c73624656c044a48fbb133faae0e26ebe0041
Tao Wen <taowen@gmail.com>
2012-11-08 23:25:57 +0800

    split local_deployer out of remote_deployer

--- src/veil/environment/local_deployer/local_deployer.py
+++ src/veil/environment/local_deployer/local_deployer.py
@@ -2,19 +2,13 @@ from __future__ import unicode_literals, print_function, division
 from veil.frontend.cli import *
 from veil.backend.shell import *
 import time
-from fabric.colors import green
 
 @script('deploy')
 def deploy():
-    print(green('veil down'))
     shell_execute('veil down')
-    print(green('backup'))
     shell_execute('veil ljmall backup deploy_backup')
-    print(green('veil install'))
     shell_execute('veil install')
-    print(green('veil up --daemonize'))
     shell_execute('veil up --daemonize')
-    print(green('veil migrate'))
     for i in range(3):
         try:
             shell_execute('veil migrate', capture=True)
