258fdb95e5fb15dd8df1059c4338125f703707fe
Tao Wen <taowen@gmail.com>
2012-11-08 23:45:04 +0800

    fix local deployer to install its dependency first

--- src/veil/environment/local_deployer/local_deployer.py
+++ src/veil/environment/local_deployer/local_deployer.py
@@ -1,14 +1,14 @@
 from __future__ import unicode_literals, print_function, division
-from veil.environment import *
 from veil.frontend.cli import *
 from veil.backend.shell import *
 import time
 
 @script('deploy')
 def deploy():
-    if VEIL_VAR_DIR.exists():
-        shell_execute('veil down')
-        shell_execute('veil ljmall backup deploy_backup')
+    shell_execute('veil install-component veil.environment.supervisor')
+    shell_execute('veil down')
+    shell_execute('veil install-component ljmall')
+    shell_execute('veil ljmall backup deploy_backup')
     shell_execute('veil install')
     shell_execute('veil up --daemonize')
     for i in range(3):
