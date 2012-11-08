2bffc31aad6ae626ab05a1f3033424edf17b530b
Tao Wen <taowen@gmail.com>
2012-11-08 23:29:41 +0800

    only do backup if var exists

--- src/veil/environment/local_deployer/local_deployer.py
+++ src/veil/environment/local_deployer/local_deployer.py
@@ -1,12 +1,14 @@
 from __future__ import unicode_literals, print_function, division
+from veil.environment import *
 from veil.frontend.cli import *
 from veil.backend.shell import *
 import time
 
 @script('deploy')
 def deploy():
-    shell_execute('veil down')
-    shell_execute('veil ljmall backup deploy_backup')
+    if VEIL_VAR_DIR.exists():
+        shell_execute('veil down')
+        shell_execute('veil ljmall backup deploy_backup')
     shell_execute('veil install')
     shell_execute('veil up --daemonize')
     for i in range(3):
