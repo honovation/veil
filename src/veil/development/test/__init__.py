ed045171dc8e75b4e5c5e781627b33db098d2f61
Tao Wen <taowen@gmail.com>
2012-11-09 16:04:29 +0800

    reloadable fixture

--- src/veil/development/test/__init__.py
+++ src/veil/development/test/__init__.py
@@ -5,6 +5,7 @@ with veil_component.init_component(__name__):
     from .case import get_executing_test
     from .case import test_hook
     from .fixture import fixture
+    from .fixture import fixture_reloader
     from unittest import skip
     from .correctness_checker import check_correctness
 
@@ -15,6 +16,7 @@ with veil_component.init_component(__name__):
         test_hook.__name__,
         # from fixture
         fixture.__name__,
+        fixture_reloader.__name__,
         # from unittest
         skip.__name__,
         check_correctness.__name__
--- src/veil/development/test/fixture.py
+++ src/veil/development/test/fixture.py
@@ -1,19 +1,59 @@
 from __future__ import unicode_literals, print_function, division
+from veil.model.collection import *
 from .case import test_hook
 from .case import get_executing_test
 
-fixtures = {}
+
+fixtures = {} # fixture_name => normally just the id of database record
+fixture_providers = {} # fixture_name => the provider function
+fixture_types = {} # type => reloaders
 
 @test_hook
 def reset_fixtures():
     get_executing_test().addCleanup(fixtures.clear)
 
 
-def fixture(func):
-    def wrapper():
-        fixture_name = func.__name__
-        if fixture_name not in fixtures:
-            fixtures[fixture_name] = func()
-        return fixtures[fixture_name]
+def fixture(fixture_type, fixture_name=None):
+    return FixtureProviderDecorator(fixture_type, fixture_name)
 
-    return wrapper
+
+class FixtureProviderDecorator(object):
+    def __init__(self, fixture_type, fixture_name=None):
+        self.fixture_type = fixture_type
+        self.fixture_name = fixture_name
+
+    def __call__(self, provider):
+        return FixtureProvider(self.fixture_type, self.fixture_name or provider.__name__, provider)
+
+
+class FixtureProvider(object):
+    def __init__(self, fixture_type, fixture_name, provider):
+        self.fixture_type = fixture_type
+        self.fixture_name = fixture_name
+        self.provider = provider
+        if fixture_name in fixture_providers:
+            raise Exception('duplicated fixture')
+        else:
+            fixture_providers[fixture_name] = self
+
+    def __call__(self):
+        if self.fixture_name not in fixtures:
+            args = self.provider()
+            fixtures[self.fixture_name] = args if isinstance(args, (list, tuple)) else [args]
+        return reload_fixture(self.fixture_type, self.fixture_name)
+
+
+def reload_fixture(fixture_type, fixture_name):
+    reloaders = fixture_types[fixture_type]
+    fixture = {}
+    for reloader in reloaders:
+        fixture.update(reloader(*fixtures[fixture_name]))
+    return objectify(fixture)
+
+
+def fixture_reloader(fixture_type):
+    def decorator(func):
+        fixture_types.setdefault(fixture_type, []).append(func)
+        return func
+
+    return decorator
