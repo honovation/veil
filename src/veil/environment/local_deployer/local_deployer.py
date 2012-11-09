5431eae73d9e567e8e70a2e6f34dbfc9daacf6d5
Tao Wen <taowen@gmail.com>
2012-11-09 18:12:06 +0800

    skip backup

--- src/veil/environment/local_deployer/local_deployer.py
+++ src/veil/environment/local_deployer/local_deployer.py
@@ -7,8 +7,8 @@ import time
 def deploy():
     shell_execute('veil install-supervisor')
     shell_execute('veil down')
-    shell_execute('veil install-component ljmall')
-    shell_execute('veil ljmall backup deploy_backup')
+#    shell_execute('veil install-component ljmall')
+#    shell_execute('veil ljmall backup deploy_backup')
     shell_execute('veil install')
     shell_execute('veil up --daemonize')
     for i in range(3):
