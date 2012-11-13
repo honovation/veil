a60afa847b2bacd0208207adb02ce30e742c0fe1
Tao Wen <taowen@gmail.com>
2012-11-13 21:37:26 +0800

    make profile a component, so the dependency can be transitive

---
+++ src/veil/profile/setting/__init__.py
@@ -0,0 +1,14 @@
+import veil_component
+
+with veil_component.init_component(__name__):
+    from veil.environment import *
+    from veil.environment.setting import *
+    from veil.environment.supervisor_setting import supervisor_settings
+    from veil.backend.database.postgresql_setting import postgresql_settings
+    from veil.backend.bucket_setting import bucket_settings
+    from veil.backend.database.db2_setting import db2_settings
+    from veil.frontend.website_setting import website_settings
+    from veil.backend.queue_setting import queue_settings
+    from veil.backend.redis_setting import redis_settings
+    from veil.backend.web_service_setting import web_service_settings
+    from veil.utility.captcha_setting import captcha_settings
