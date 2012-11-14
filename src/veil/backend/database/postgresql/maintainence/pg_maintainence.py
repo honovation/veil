7052904c91e7fa35c618304a22b8247c1a974e48
Tao Wen <taowen@gmail.com>
2012-11-14 13:04:17 +0800

    fix migration check

--- src/veil/backend/database/postgresql/maintainence/pg_maintainence.py
+++ src/veil/backend/database/postgresql/maintainence/pg_maintainence.py
@@ -50,7 +50,8 @@ def migrate_all():
 
 @script('migrate')
 def migrate(purpose):
-    if not is_current_veil_server_hosting('{}_purpose'.format(purpose)):
+    if not is_current_veil_server_hosting('{}_postgresql'.format(purpose)):
+        print('[MIGRATE] skip {}, as not hosted by this server'.format(purpose))
         return
     wait_for_server_up(purpose)
     create_database_if_not_exists(purpose)
