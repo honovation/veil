e5d729e5c7cec615f8232963f0994216fbf56e49
Tao Wen <taowen@gmail.com>
2012-11-21 23:09:30 +0800

    rename old dirs as part of deployment

--- src/veil/environment/local_deployer/local_deployer.py
+++ src/veil/environment/local_deployer/local_deployer.py
@@ -1,13 +1,37 @@
 from __future__ import unicode_literals, print_function, division
+import logging
+import os
+from veil.environment import *
 from veil.frontend.cli import *
 from veil.utility.shell import *
+
+LOGGER = logging.getLogger(__name__)
 
 @script('deploy')
 def deploy():
     shell_execute('veil install component?veil.environment.supervisor')
     shell_execute('veil down')
+    rename_old_dirs()
     shell_execute('veil install component?ljmall')
     shell_execute('veil ljmall backup deploy_backup')
     shell_execute('veil install-server')
     shell_execute('veil up --daemonize')
     shell_execute('veil migrate')
+
+
+@script('rename-old-dirs')
+def rename_old_dirs():
+    rename_old_dir(VEIL_VAR_DIR)
+    rename_old_dir(VEIL_ETC_DIR)
+
+
+def rename_old_dir(dir):
+    existing_names = os.listdir(dir)
+    for file in VEIL_VAR_DIR.listdir():
+        if '_' in file.name:
+            new_name = file.name.replace('_', '-')
+            if new_name in existing_names:
+                LOGGER.info('skip rename {} to {}, as same name exists'.format(file.name, new_name))
+            else:
+                LOGGER.info('rename {} to {}'.format(file.name, new_name))
+                file.rename(dir / new_name)
