1bc86aebd29057c570193a427b25dad8aa3e2741
Tao Wen <taowen@gmail.com>
2012-11-28 00:45:35 +0800

    add syslog testing code

--- src/veil_component/component_logging.py
+++ src/veil_component/component_logging.py
@@ -1,6 +1,9 @@
 import logging
+import logging.handlers
+import socket
 import os
 import sys
+import json
 from .component_map import get_root_component
 
 logging_levels = None
@@ -38,7 +41,25 @@ def configure_root_component_logger(component_name):
     if component_name in configured_root_loggers:
         return
     configured_root_loggers.add(component_name)
-    handler = logging.StreamHandler(os.fdopen(sys.stderr.fileno(), 'w', 0))
-    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    console_handler = logging.StreamHandler(os.fdopen(sys.stderr.fileno(), 'w', 0))
+    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    syslog_handler = logging.handlers.SysLogHandler(
+        address='/dev/veil-log',
+        facility=logging.handlers.SysLogHandler.LOG_USER)
+    syslog_handler.setFormatter(EventFormatter())
+    syslog_handler.addFilter(EventFilter())
     logger = logging.getLogger(component_name)
-    logger.addHandler(handler)
+    logger.addHandler(console_handler)
+#    logger.addHandler(syslog_handler)
+
+class EventFilter(logging.Filter):
+    def filter(self, record):
+        return ':' in record.message
+
+class EventFormatter(logging.Formatter):
+    def format(self, record):
+        event_name = record.message.split(':')[0]
+        event = {'__category__': record.name, '__name__': event_name}
+        if record.args and isinstance(record.args, dict):
+            event.update(record.args)
+        return json.dumps(event)
