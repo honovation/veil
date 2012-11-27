38f2f146955b519b2c4c65f4683a6e3b943bc538
Eric Du <eric@honovation.com>
2012-11-27 17:21:41 +0800

    keep checking after exception occurrs during checking if supervisord is running

--- src/veil/environment/supervisor/supervisorctl.py
+++ src/veil/environment/supervisor/supervisorctl.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals, print_function, division
+import logging
 from veil.environment import *
-from veil.environment.setting import *
 from veil.utility.shell import *
+
+LOGGER = logging.getLogger(__name__)
 
 def supervisorctl(action, *arguments, **kwargs):
     return shell_execute('veil execute supervisorctl -c {} {} {}'.format(
@@ -19,10 +21,12 @@ def is_supervisord_running():
         output = shell_execute('veil execute supervisorctl -c {} {}'.format(
             VEIL_ETC_DIR / 'supervisor.cfg', 'status'), capture=True)
         return 'refused' not in output
-    except ShellExecutionError, e:
-        if 'SHUTDOWN_STATE' in e.output:
+    except Exception, e:
+        if isinstance(e, ShellExecutionError) and 'SHUTDOWN_STATE' in e.output:
             return True
-        raise
+        LOGGER.exception('Exception occurred while checking if supervisord is running')
+        return True
+
 
 def are_all_supervisord_programs_running():
     for line in supervisorctl('status', capture=True).split('\n'):
