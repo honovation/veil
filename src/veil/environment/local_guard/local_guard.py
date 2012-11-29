f360aa9b717e135028c6fc91836d9a93976a1850
Tao Wen <taowen@gmail.com>
2012-11-29 14:42:39 +0800

    add backup to veil command

--- src/veil/environment/local_guard/local_guard.py
+++ src/veil/environment/local_guard/local_guard.py
@@ -9,6 +9,8 @@ from veil.environment.supervisor import *
 def backup(backup_path):
     if is_supervisord_running():
         raise Exception('can not backup while supervisord is executing')
-    os.makedirs(os.path.dirname(backup_path), mode=0755)
+    backup_dir = os.path.dirname(backup_path)
+    if not os.path.exists(backup_dir):
+        os.makedirs(backup_dir, mode=0755)
     shell_execute('tar czf {} --exclude *.pid -C {} .'.format(
         backup_path, VEIL_VAR_DIR))
--- src/veil/environment/remote_guard/remote_backup.py
+++ src/veil/environment/remote_guard/remote_backup.py
@@ -6,38 +6,38 @@ from veil.frontend.cli import *
 from veil.environment import *
 
 @script('backup-env')
-def backup_env():
-    for veil_server_name in sorted(get_veil_servers(VEIL_ENV).keys()):
-        bring_down_server(veil_server_name)
-    for veil_server_name in sorted(get_veil_servers(VEIL_ENV).keys()):
-        backup_server(veil_server_name)
-    for veil_server_name in sorted(get_veil_servers(VEIL_ENV).keys()):
-        bring_up_server(veil_server_name)
+def backup_env(backing_up_env):
+    for veil_server_name in sorted(get_veil_servers(backing_up_env).keys()):
+        bring_down_server(backing_up_env, veil_server_name)
+    for veil_server_name in sorted(get_veil_servers(backing_up_env).keys()):
+        backup_server(backing_up_env, veil_server_name)
+    for veil_server_name in sorted(get_veil_servers(backing_up_env).keys()):
+        bring_up_server(backing_up_env, veil_server_name)
 
 
-def bring_down_server(veil_server_name):
-    deployed_via = get_remote_veil_server(VEIL_ENV, veil_server_name).deployed_via
+def bring_down_server(backing_up_env, veil_server_name):
+    deployed_via = get_remote_veil_server(backing_up_env, veil_server_name).deployed_via
     fabric.api.env.host_string = deployed_via
-    with fabric.api.cd('/opt/{}/app'.format(VEIL_ENV)):
-        fabric.api.sudo('veil :{}/{} down'.format(VEIL_ENV, veil_server_name))
+    with fabric.api.cd('/opt/{}/app'.format(backing_up_env)):
+        fabric.api.sudo('veil :{}/{} down'.format(backing_up_env, veil_server_name))
 
 
-def bring_up_server(veil_server_name):
-    deployed_via = get_remote_veil_server(VEIL_ENV, veil_server_name).deployed_via
+def bring_up_server(backing_up_env, veil_server_name):
+    deployed_via = get_remote_veil_server(backing_up_env, veil_server_name).deployed_via
     fabric.api.env.host_string = deployed_via
-    with fabric.api.cd('/opt/{}/app'.format(VEIL_ENV)):
-        fabric.api.sudo('veil :{}/{} up'.format(VEIL_ENV, veil_server_name))
+    with fabric.api.cd('/opt/{}/app'.format(backing_up_env)):
+        fabric.api.sudo('veil :{}/{} up'.format(backing_up_env, veil_server_name))
 
 
-def backup_server(veil_server_name):
-    deployed_via = get_remote_veil_server(VEIL_ENV, veil_server_name).deployed_via
+def backup_server(backing_up_env, veil_server_name):
+    deployed_via = get_remote_veil_server(backing_up_env, veil_server_name).deployed_via
     fabric.api.env.host_string = deployed_via
     backup_path = '/backup/{}-{}-{}.tar.gz'.format(
-        VEIL_ENV, veil_server_name,
+        backing_up_env, veil_server_name,
         datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
-    with fabric.api.cd('/opt/{}/app'.format(VEIL_ENV)):
+    with fabric.api.cd('/opt/{}/app'.format(backing_up_env)):
         fabric.api.sudo('veil :{}/{} backup {}'.format(
-            VEIL_ENV, veil_server_name,
+            backing_up_env, veil_server_name,
             backup_path))
     os.mkdir('/backup', mode=0755)
     fabric.api.get(backup_path, backup_path)
