a8f7ed4dbedf66bbc4b081bf5eddd1f6212ac121
Tao Wen <taowen@gmail.com>
2012-11-30 00:32:05 +0800

    simplify the source code monitor, hopefully more reliable

--- src/veil/development/source_code_monitor/source_code_monitor.py
+++ src/veil/development/source_code_monitor/source_code_monitor.py
@@ -15,7 +15,6 @@ modify_times = {}
 
 @script('up')
 def bring_up_source_code_monitor():
-    restarts_all = False
     for i in range(10):
         try:
             load_reloads_on_change_programs_components()
@@ -23,12 +22,9 @@ def bring_up_source_code_monitor():
         except:
             LOGGER.info('code contains error, retry in 10 seconds...')
             time.sleep(10) # give 10 seconds to you to fix the problem
-            restarts_all = True # once fixed, ensure all restarted
+    load_reloads_on_change_programs_components()
+    restart_all()
     LOGGER.info('start monitoring source code changes...')
-    if restarts_all:
-        restart_all()
-    else:
-        start_all()
     while True:
         reload_on_change()
 
@@ -53,23 +49,13 @@ def list_reloads_on_change_programs():
     return programs
 
 
-def reload_on_change(exit_on_no_change=False):
+def reload_on_change():
     modified_path = is_source_code_modified()
     if modified_path:
         LOGGER.info('{} modified, reloading...'.format(modified_path))
-        refresh_modify_times()
-        restart_all()
-        reload_on_change(exit_on_no_change=True) # refresh the state
+        sys.exit(1)
     else:
-        if exit_on_no_change:
-            sys.exit(1)
-        else:
-            time.sleep(1)
-
-
-def refresh_modify_times():
-    modify_times.clear()
-    is_source_code_modified()
+        time.sleep(1)
 
 
 def is_source_code_modified():
@@ -96,10 +82,6 @@ def is_source_code_modified():
 
 def restart_all():
     execute('restart')
-
-
-def start_all():
-    execute('start')
 
 
 def execute(action):
