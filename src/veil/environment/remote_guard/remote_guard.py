352a3f2c832eac1a09c920f9f42f25577ae7b4bd
Tao Wen <taowen@gmail.com>
2012-11-30 17:29:51 +0800

    add remote_guard_program

--- src/veil/environment/remote_guard/remote_guard.py
+++ src/veil/environment/remote_guard/remote_guard.py
@@ -2,8 +2,31 @@ from __future__ import unicode_literals, print_function, division
 import fabric.api
 import datetime
 import os
+import logging
+import time
+from croniter.croniter import croniter
 from veil.frontend.cli import *
 from veil.environment import *
+from veil.utility.clock import get_current_timestamp
+
+LOGGER = logging.getLogger(__name__)
+
+@script('backup-env-at')
+def backup_env_at(backing_up_env, crontab_expression):
+    with open('{}/.backup-{}.pass'.format(os.getenv('HOME'), backing_up_env)) as f:
+        user, password = f.read().split(':')
+    fabric.api.user = user
+    fabric.api.password = password
+    while True:
+        now = get_current_timestamp()
+        next = croniter(crontab_expression, now).get_next()
+        delta = next - now
+        LOGGER.info('backup later: wake up in %(delta)s seconds', {
+            'delta': delta
+        })
+        time.sleep(delta)
+        backup_env(backing_up_env)
+
 
 @script('backup-env')
 def backup_env(backing_up_env):
@@ -11,7 +34,8 @@ def backup_env(backing_up_env):
         bring_down_server(backing_up_env, veil_server_name)
     timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
     for veil_server_name in sorted(get_veil_servers(backing_up_env).keys()):
-        backup_server(backing_up_env, veil_server_name, timestamp)
+        if '@guard' != veil_server_name:
+            backup_server(backing_up_env, veil_server_name, timestamp)
     for veil_server_name in sorted(get_veil_servers(backing_up_env).keys()):
         bring_up_server(backing_up_env, veil_server_name)
 
---
+++ src/veil/environment/remote_guard_setting.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals, print_function, division
+from veil.model.collection import *
+
+def remote_guard_program(backing_up_env, crontab_expression):
+    return objectify({
+        'remote_guard': {
+            'execute_command': 'veil environment remote-guard backup-env-at {} {}'.format(
+                backing_up_env, crontab_expression),
+            'resources': [('veil_installer.component_resource', {
+                'name': 'veil.environment.remote_guard'
+            })]
+        }
+    })
