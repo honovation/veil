116fc484b29d0c1a333c41c7d7dd3b17927ce654
Tao Wen <taowen@gmail.com>
2012-12-01 20:41:59 +0800

    fix reference to veil.environment.supervisor

--- src/veil/server/in_service/local_deployer.py
+++ src/veil/server/in_service/local_deployer.py
@@ -7,7 +7,7 @@ LOGGER = logging.getLogger(__name__)
 
 @script('deploy')
 def deploy():
-    shell_execute('veil install veil_installer.component_resource?veil.environment.supervisor')
+    shell_execute('veil install veil_installer.component_resource?veil.server.supervisor')
     shell_execute('veil down')
     shell_execute('veil install-server')
     shell_execute('veil up --daemonize')
