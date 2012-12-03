36f515760a33be56b60d1f0aaff76f1f0bedf031
Tao Wen <taowen@gmail.com>
2012-12-03 22:43:51 +0800

    fix reference of lxc resources

--- src/veil/environment/lxc/__init__.py
+++ src/veil/environment/lxc/__init__.py
@@ -2,21 +2,17 @@ import veil_component
 
 with veil_component.init_component(__name__):
     from .lxc_container_installer import lxc_container_resource
-    from .lxc_container_installer import lxc_container_running_resource
-    from .lxc_container_installer import lxc_container_stopped_resource
+    from .lxc_container_installer import lxc_container_in_service_resource
     from .lxc_container_installer import lxc_container_ready_resource
     from .lxc_container_user_installer import lxc_container_user_resource
-    from .lxc_container_user_installer import lxc_container_user_password_resource
     from .lxc_container_user_installer import lxc_container_user_group_resource
 
     __all__ = [
         # from lxc_container_installer
         lxc_container_resource.__name__,
-        lxc_container_running_resource.__name__,
-        lxc_container_stopped_resource.__name__,
+        lxc_container_in_service_resource.__name__,
         lxc_container_ready_resource.__name__,
         # from lxc_container_user_installer
         lxc_container_user_resource.__name__,
-        lxc_container_user_password_resource.__name__,
         lxc_container_user_group_resource.__name__
     ]
