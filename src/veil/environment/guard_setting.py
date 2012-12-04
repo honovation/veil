363ae8721fa5af8a95d45e07e9d548b29d2db025
Tao Wen <taowen@gmail.com>
2012-12-05 02:41:17 +0800

    run guard_program as root

--- src/veil/environment/guard_setting.py
+++ src/veil/environment/guard_setting.py
@@ -5,6 +5,7 @@ def guard_program(crontab_expression):
     return objectify({
         'guard': {
             'execute_command': "veil environment backup guard-up '{}'".format(crontab_expression),
+            'run_as': 'root',
             'resources': [('veil_installer.component_resource', {
                 'name': 'veil.environment.backup'
             })]
