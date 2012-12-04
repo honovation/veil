7114b61132bb4b5c2df52eb12bcee394f21c22ce
Tao Wen <taowen@gmail.com>
2012-12-04 16:45:47 +0800

    backup to /backup/{time}

--- src/veil/environment/backup/env_backup.py
+++ src/veil/environment/backup/env_backup.py
@@ -18,11 +18,13 @@ def env_backup_resource():
     veil_server_names.remove('@guard')
     for veil_server_name in veil_server_names:
         bring_down_server(VEIL_ENV, veil_server_name)
-    timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-    for veil_server_name in veil_server_names:
-        backup_server(VEIL_ENV, veil_server_name, timestamp)
-    for veil_server_name in veil_server_names:
-        bring_up_server(VEIL_ENV, veil_server_name)
+    try:
+        timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        for veil_server_name in veil_server_names:
+            backup_server(VEIL_ENV, veil_server_name, timestamp)
+    finally:
+        for veil_server_name in veil_server_names:
+            bring_up_server(VEIL_ENV, veil_server_name)
 
 
 def bring_down_server(backing_up_env, veil_server_name):
@@ -41,12 +43,14 @@ def bring_up_server(backing_up_env, veil_server_name):
 
 def backup_server(backing_up_env, veil_server_name, timestamp):
     fabric.api.env.host_string = get_veil_server_deploys_via(backing_up_env, veil_server_name)
-    backup_path = '/backup/{}-{}-{}.tar.gz'.format(
-        backing_up_env, veil_server_name, timestamp)
+    backup_path = '/backup/{}/{}-{}-{}.tar.gz'.format(
+        timestamp, backing_up_env, veil_server_name, timestamp)
     with fabric.api.cd('/opt/{}/app'.format(backing_up_env)):
         fabric.api.sudo('veil :{}/{} backup {}'.format(
             backing_up_env, veil_server_name,
             backup_path))
     if not os.path.exists('/backup'):
         os.mkdir('/backup', 0755)
+    if not os.path.exists('/backup/{}'.format(timestamp)):
+        os.mkdir('/backup/{}'.format(timestamp), 0755)
     fabric.api.get(backup_path, backup_path)
