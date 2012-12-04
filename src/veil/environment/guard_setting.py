95fcc29269800cd92e21fea29f4f7c99ecfdebbe
Tao Wen <taowen@gmail.com>
2012-12-04 15:15:19 +0800

    fix guard-up

--- src/veil/environment/guard_setting.py
+++ src/veil/environment/guard_setting.py
@@ -4,7 +4,7 @@ from veil.model.collection import *
 def guard_program(crontab_expression):
     return objectify({
         'guard': {
-            'execute_command': "veil environment backup guard-up {} '{}'".format(crontab_expression),
+            'execute_command': "veil environment backup guard-up '{}'".format(crontab_expression),
             'resources': [('veil_installer.component_resource', {
                 'name': 'veil.environment.backup'
             })]
