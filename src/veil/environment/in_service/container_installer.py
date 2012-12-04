f88a6b02815857c4bacca890cefc37b197352c75
Tao Wen <taowen@gmail.com>
2012-12-04 13:57:19 +0800

    install veil_host.resources

--- src/veil/environment/in_service/container_installer.py
+++ src/veil/environment/in_service/container_installer.py
@@ -31,9 +31,7 @@ def veil_server_container_resource(veil_env_name, veil_server_name):
     veil_host = get_veil_host(veil_env_name, veil_server.hosted_on)
     fabric.state.env.host_string = '{}:{}'.format(veil_host.ssh_ip, veil_host.ssh_port)
     fabric.state.env.user = veil_host.ssh_user
-    veil_server_user_name = veil_host.ssh_user
-    installer_file_content = render_installer_file(
-        veil_env_name, veil_server_name, veil_server.sequence_no, veil_server_user_name)
+    installer_file_content = render_installer_file(veil_env_name, veil_server_name)
     installer_file_path = '/opt/INSTALLER-{}-{}'.format(veil_env_name, veil_server_name)
     is_installed = installer_file_content == remote_get_content('{}.installed'.format(installer_file_path))
     dry_run_result = get_dry_run_result()
@@ -68,7 +66,8 @@ def veil_server_container_config_resource(veil_env_name, veil_server_name, serve
             remote_path='/home/{}/.ssh/authorized_keys'.format(veil_server_user_name),
             owner=veil_server_user_name, owner_group=veil_server_user_name, mode=0644),
         veil_server_container_file_resource(
-            local_path=server_config_dir / 'known_hosts', veil_env_name=veil_env_name, veil_server_name=veil_server_name,
+            local_path=server_config_dir / 'known_hosts', veil_env_name=veil_env_name,
+            veil_server_name=veil_server_name,
             remote_path='/home/{}/.ssh/known_hosts'.format(veil_server_user_name),
             owner=veil_server_user_name, owner_group=veil_server_user_name, mode=0644),
         veil_server_container_directory_resource(
@@ -76,7 +75,8 @@ def veil_server_container_config_resource(veil_env_name, veil_server_name, serve
             remote_path='/root/.ssh',
             owner='root', owner_group='root', mode=0755),
         veil_server_container_file_resource(
-            local_path=server_config_dir / 'known_hosts', veil_env_name=veil_env_name, veil_server_name=veil_server_name,
+            local_path=server_config_dir / 'known_hosts', veil_env_name=veil_env_name,
+            veil_server_name=veil_server_name,
             remote_path='/root/.ssh/known_hosts',
             owner=veil_server_user_name, owner_group=veil_server_user_name, mode=0644)]
     if (server_config_dir / 'config').exists():
@@ -136,16 +136,24 @@ def remote_put_content(remote_path, content, **kwargs):
     fabric.api.put(temp_file_path, remote_path, **kwargs)
 
 
-def render_installer_file(veil_env_name, veil_server_name, sequence_no, user_name):
+def render_installer_file(veil_env_name, veil_server_name):
+    veil_server = get_veil_server(veil_env_name, veil_server_name)
+    veil_host = get_veil_host(veil_env_name, veil_server.hosted_on)
+    veil_server_user_name = veil_host.ssh_user
     container_name = '{}-{}'.format(veil_env_name, veil_server_name)
-    mac_address = '00:16:3e:73:bb:{}'.format(sequence_no)
-    ip_address = '10.0.3.{}'.format(sequence_no)
+    mac_address = '00:16:3e:73:bb:{}'.format(veil_server.sequence_no)
+    ip_address = '10.0.3.{}'.format(veil_server.sequence_no)
     iptables_rule = 'PREROUTING -p tcp -m tcp --dport {}22 -j DNAT --to-destination 10.0.3.{}:22'.format(
-        sequence_no, sequence_no)
-    return render_config(
+        veil_server.sequence_no, veil_server.sequence_no)
+    installer_file_content = render_config(
         'container-installer-file.j2', mac_address=mac_address, ip_address=ip_address,
-        iptables_rule=iptables_rule, container_name=container_name, user_name=user_name)
-
+        iptables_rule=iptables_rule, container_name=container_name, user_name=veil_server_user_name)
+    lines = [installer_file_content]
+    for resource in veil_host.resources:
+        installer_name, installer_args = resource
+        line = '{}?{}'.format(installer_name, '&'.join(['{}={}'.format(k, v) for k, v in installer_args.items()]))
+        lines.append(line)
+    return '\n'.join(lines)
 
 
 
