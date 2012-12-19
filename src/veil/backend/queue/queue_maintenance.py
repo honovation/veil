45bf79832eab42db5e2e5451a4300264c0299836
Tao Wen <taowen@gmail.com>
2012-12-19 15:16:13 +0800

    add delete-failed-jobs

---
+++ src/veil/backend/queue/queue_maintenance.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals, print_function, division
+import pyres.failure
+import pyres
+import base64
+from redis.client import Redis
+from veil.frontend.cli import *
+from .queue_client_installer import load_queue_client_config
+
+@script('count-failed-jobs')
+def count_failed_jobs(queue):
+    count = 0
+    for job in pyres.failure.all(get_resq(), 0, -1):
+        if queue == job['queue']:
+            count += 1
+    print(count)
+
+
+@script('delete-failed-jobs')
+def count_failed_jobs(queue):
+    resq = get_resq()
+    count = 0
+    for job in pyres.failure.all(resq, 0, -1):
+        if queue == job['queue']:
+            pyres.failure.delete(resq, base64.decodestring(job['redis_value']))
+            count += 1
+    print(count)
+
+
+def get_resq():
+    config = load_queue_client_config()
+    redis = Redis(host=config.host, port=config.port)
+    resq = pyres.ResQ(server=redis)
+    return resq
