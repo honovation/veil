ae29a9cf465ace8055b5638491de35f891980ceb
Tao Wen <taowen@gmail.com>
2012-12-26 02:26:55 +0800

    delete default user ubuntu

--- src/veil/environment/lxc/lxc_container_user_installer.py
+++ src/veil/environment/lxc/lxc_container_user_installer.py
@@ -7,23 +7,36 @@ from veil.utility.shell import *
 LOGGER = logging.getLogger(__name__)
 
 @atomic_installer
-def lxc_container_user_resource(container_name, user_name):
+def lxc_container_user_resource(container_name, user_name, state='created'):
     rootfs_path = '/var/lib/lxc/{}/rootfs'.format(container_name)
     try:
-        is_installed = unsafe_call('chroot {} getent passwd {}'.format(rootfs_path, user_name))
+        is_user_existing = unsafe_call('chroot {} getent passwd {}'.format(rootfs_path, user_name))
     except:
-        is_installed = False
+        is_user_existing = False
+    if 'created' == state:
+        action = '-' if is_user_existing else 'CREATE'
+    elif 'deleted' == state:
+        action = 'DELETE' if is_user_existing else '-'
+    else:
+        raise Exception('unknown state: {}'.format(state))
     dry_run_result = get_dry_run_result()
     if dry_run_result is not None:
         key = 'lxc_container_user?container_name={}&user_name={}'.format(container_name, user_name)
-        dry_run_result[key] = '-' if is_installed else 'INSTALL'
+        dry_run_result[key] = action
         return
-    if not is_installed:
+    if 'CREATE' == action:
         LOGGER.info('create lxc container user: %(user_name)s in container %(container_name)s...', {
             'user_name': user_name,
             'container_name': container_name
         })
         shell_execute('chroot {} useradd --create-home -s /bin/bash {}'.format(rootfs_path, user_name), capture=True)
+    elif 'DELETE' == action:
+        LOGGER.info('delete lxc container user: %(user_name)s in container %(container_name)s...', {
+            'user_name': user_name,
+            'container_name': container_name
+        })
+        shell_execute('chroot {} userdel -r {}'.format(rootfs_path, user_name), capture=True)
+
 
 
 @atomic_installer
