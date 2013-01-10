446645d7125c218e37f7564c598cfa9540520812
Tao Wen <taowen@gmail.com>
2013-01-10 13:47:19 +0800

    deploy dns config to container

--- src/veil/environment/lxc/lxc_container_network_installer.py
+++ src/veil/environment/lxc/lxc_container_network_installer.py
@@ -25,3 +25,23 @@ def lxc_container_network_resource(container_name, ip_address, gateway):
         'gateway': gateway
     })
     NETWORK_INTERFACES_PATH.write_text(config_content)
+
+
+@atomic_installer
+def lxc_container_dns_resource(container_name, dns):
+    CONTAINER_ROOTFS_PATH = as_path('/var/lib/lxc/') / container_name / 'rootfs'
+    RESOLVE_CONF_PATH = CONTAINER_ROOTFS_PATH / 'etc' / 'resolvconf' / 'resolv.conf.d' / 'tail'
+    config_content = 'nameserver {}'.format(dns)
+    is_installed = config_content == RESOLVE_CONF_PATH.text()
+    dry_run_result = get_dry_run_result()
+    if dry_run_result is not None:
+        key = 'lxc_container_dns_resource?container_name={}&dns={}'.find(container_name, dns)
+        dry_run_result[key] = '-' if is_installed else 'INSTALL'
+        return
+    if is_installed:
+        return
+    LOGGER.info('set container dns: in %(container_name)s to %(dns)s', {
+        'container_name': container_name,
+        'dns': dns
+    })
+    RESOLVE_CONF_PATH.write_text(config_content)
