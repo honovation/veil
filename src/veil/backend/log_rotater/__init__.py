230350c8e6d26123c2b9b667df4b90d9f8ef07f5
Tao Wen <taowen@gmail.com>
2013-01-17 03:48:25 +0800

    expose log_rotater_resource

---
+++ src/veil/backend/log_rotater/__init__.py
@@ -0,0 +1,8 @@
+import veil_component
+
+with veil_component.init_component(__name__):
+    from .log_rotater_installer import log_rotater_resource
+
+    __all__ = [
+        log_rotater_resource.__name__
+    ]
