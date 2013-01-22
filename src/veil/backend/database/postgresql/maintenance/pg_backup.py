49a2c47d04692f355f819df227bb4f6aaa7dc593
Tao Wen <taowen@gmail.com>
2013-01-22 14:29:20 +0800

    restore postgresql backup using owner

--- src/veil/backend/database/postgresql/maintenance/pg_backup.py
+++ src/veil/backend/database/postgresql/maintenance/pg_backup.py
@@ -3,6 +3,7 @@ import os
 from veil.frontend.cli import *
 from veil.backend.database.client import *
 from veil.utility.shell import *
+from ..server.pg_server_installer import load_postgresql_maintenance_config
 
 #pg_dump --host 10.24.3.10 --port 5432 --username "veil" --no-password  --format tar --blobs --verbose --file "/home/dejavu/dump" "ljmall"
 #pg_restore --host localhost --port 5432 --username "veil" --no-password  --format tar --verbose -c -d ljmall /home/dejavu/dump.tar
@@ -22,11 +23,12 @@ def create_backup(purpose, backup_path):
 @script('restore-backup')
 def restore_backup(backup_path, purpose):
     config = load_database_client_config(purpose)
+    maintenance_config = load_postgresql_maintenance_config(purpose)
     env = os.environ.copy()
-    env['PGPASSWORD'] = config.password
+    env['PGPASSWORD'] = maintenance_config.owner_password
     shell_execute('pg_restore --host {host} --port {port} --username {user} --format tar --verbose -c -d {database} "{backup_path}"'.format(
         host=config.host,
         port=config.port,
-        user=config.user,
+        user=maintenance_config.owner,
         backup_path=backup_path,
         database=config.database), env=env)
