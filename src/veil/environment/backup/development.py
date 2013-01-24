633c5be222d2ff29ff8c3ff2cbbd962f0fccc813
Tao Wen <taowen@gmail.com>
2013-01-24 11:22:56 +0800

    restore from baseline can only restore db

--- src/veil/environment/backup/development.py
+++ src/veil/environment/backup/development.py
@@ -8,19 +8,19 @@ from veil.utility.shell import *
 BASELINE_DIR = VEIL_HOME / 'baseline'
 
 @script('download-baseline')
-def download_baseline(veil_env_name):
+def download_baseline(veil_env_name, pattern):
     if BASELINE_DIR.exists():
         BASELINE_DIR.rmtree()
     BASELINE_DIR.mkdir()
     fabric.state.env.host_string = get_veil_server_deploys_via(veil_env_name, '@guard')
     dir = fabric.api.run('readlink /backup/latest')
-    fabric.api.get('{}/*'.format(dir), VEIL_HOME / 'baseline')
+    fabric.api.get('{}/{}'.format(dir, '*{}*'.format(pattern) if pattern else '*'), VEIL_HOME / 'baseline')
 
 
 @script('restore-from-baseline')
-def restore_from_baseline(veil_env_name=None):
+def restore_from_baseline(veil_env_name=None, pattern=''):
     if veil_env_name:
-        download_baseline(veil_env_name)
+        download_baseline(veil_env_name, pattern)
     else:
         if not BASELINE_DIR.exists():
             raise Exception('baseline not downloaded yet, pass env name to restore-from-baseline to download')
