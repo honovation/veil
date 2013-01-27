7e8cd1a10c369119964a705fd82b051de3dad281
Eric Du <eric@honovation.com>
2013-01-27 18:08:50 +0800

    implement format_timedelta and replace it with babel.dates.format_timedelta when babel v1 is released

--- src/veil/frontend/locale/l10n.py
+++ src/veil/frontend/locale/l10n.py
@@ -1,23 +1,30 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function, division
-import babel.dates
-import pytz
 import datetime
+import pytz
+import babel.dates
 from veil.utility.clock import DEFAULT_CLIENT_TIMEZONE
 from veil.frontend.template import template_filter
 from veil.utility.clock import get_current_time
 from .i18n import get_current_locale, _
 
 @template_filter('timedelta')
-def timedelta_filter(value):
-    current_time = get_current_time()
-    if current_time > value:
-        delta = current_time - value
-        direction = _(' ago')
+def timedelta_filter(delta, granularity='second', with_direction=False):
+    if with_direction:
+        if delta >= datetime.timedelta(seconds=0):
+            direction = _(' 后')
+        else:
+            direction = _(' 前')
     else:
-        delta = value - current_time
-        direction = _(' later')
-    delta_str = babel.dates.format_timedelta(delta, locale=get_current_locale())
+        direction = ''
+    delta_str = format_timedelta(delta, granularity=granularity, locale=get_current_locale())
     return '{}{}'.format(delta_str, direction)
+
+
+@template_filter('timedelta_by_now')
+def timedelta_by_now_filter(value, granularity='second'):
+    current_time = get_current_time()
+    return timedelta_filter(value - current_time, granularity, with_direction=True)
 
 
 @template_filter('time')
@@ -46,3 +53,63 @@ def parse_epoch_datetime(text):
     if text:
         return datetime.datetime.utcfromtimestamp(float(text)).replace(tzinfo=pytz.utc)
     return None
+
+TIMEDELTA_UNITS = (
+    ('year', '年', 3600 * 24 * 365),
+    ('month', '月', 3600 * 24 * 30),
+    ('week', '周', 3600 * 24 * 7),
+    ('day', '天', 3600 * 24),
+    ('hour', '小时', 3600),
+    ('minute', '分钟', 60),
+    ('second', '秒', 1)
+    )
+
+def format_timedelta(delta, granularity='second', threshold=.85, locale='zh_CN'):
+    """Return a time delta according to the rules of the given locale.
+    >>> format_timedelta(timedelta(weeks=12), locale='en_US')
+    '3 mths'
+    >>> format_timedelta(timedelta(seconds=1), locale='es')
+    '1 s'
+    The granularity parameter can be provided to alter the lowest unit
+    presented, which defaults to a second.
+
+    >>> format_timedelta(timedelta(hours=3), granularity='day',
+    ...                  locale='en_US')
+    '1 day'
+    The threshold parameter can be used to determine at which value the
+    presentation switches to the next higher unit. A higher threshold factor
+    means the presentation will switch later. For example:
+    >>> format_timedelta(timedelta(hours=23), threshold=0.9, locale='en_US')
+    '1 day'
+    >>> format_timedelta(timedelta(hours=23), threshold=1.1, locale='en_US')
+    '23 hrs'
+    :param delta: a ``timedelta`` object representing the time difference to
+                  format, or the delta in seconds as an `int` value
+    :param granularity: determines the smallest unit that should be displayed,
+                        the value can be one of "year", "month", "week", "day",
+                        "hour", "minute" or "second"
+    :param threshold: factor that determines at which point the presentation
+                      switches to the next higher unit
+    :param locale: a `Locale` object or a locale identifier
+    :rtype: `unicode`
+    """
+    if isinstance(delta, datetime.timedelta):
+        seconds = int((delta.days * 86400) + delta.seconds)
+    else:
+        seconds = int(delta)
+    result = ''
+    reminder = abs(seconds)
+    for unit, plural_form, secs_per_unit in TIMEDELTA_UNITS:
+        multiple = reminder // secs_per_unit
+        reminder = reminder % secs_per_unit
+        if unit == granularity:
+            if reminder / secs_per_unit >= threshold:
+                multiple += 1
+            result += '{}{}'.format(multiple, plural_form)
+            break
+        else:
+            if multiple >= 1:
+                result += '{}{}'.format(multiple, plural_form)
+                if reminder == 0:
+                    break
+    return result
