b55b3204e22f87cafc9686102ad8b2009f265452
Eric Du <eric@honovation.com>
2013-02-12 16:09:57 +0800

    job supports retry via leveraging pyres built-in support

--- src/veil/backend/queue/job.py
+++ src/veil/backend/queue/job.py
@@ -16,20 +16,24 @@ def register_job_context_manager(context_manager):
     })
 
 
-def job(queue):
+def job(queue, retry_every=None, retry_timeout=0):
     if isfunction(queue):
         job_handler = queue
-        return JobHandlerDecorator(None)(job_handler)
+        return JobHandlerDecorator(None, retry_every, retry_timeout)(job_handler)
     else:
-        return JobHandlerDecorator(queue)
+        return JobHandlerDecorator(queue, retry_every, retry_timeout)
 
 
 class JobHandlerDecorator(object):
-    def __init__(self, queue):
+    def __init__(self, queue, retry_every=None, retry_timeout=0):
         self.queue = queue
+        self.retry_every = retry_every
+        self.retry_timeout = retry_timeout
 
     def __call__(self, job_handler):
         job_handler.queue = self.queue or job_handler.__name__.replace('_job', '')
+        job_handler.retry_every = self.retry_every
+        job_handler.retry_timeout = self.retry_timeout
         job_handler.perform = lambda payload: perform(job_handler, payload)
         return job_handler
 
