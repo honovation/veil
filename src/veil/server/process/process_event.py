7619948c9af8cefc598371055fe5c4b3d255a5ef
Eric Du <eric@honovation.com>
2013-02-19 14:58:03 +0800

    register SIGINT

--- src/veil/server/process/process_event.py
+++ src/veil/server/process/process_event.py
@@ -7,10 +7,11 @@ import os
 EVENT_PROCESS_SETUP = define_event('process-setup')
 EVENT_PROCESS_TEARDOWN = define_event('process-teardown')
 
-def handle_sigterm(signum, frame):
+def handle_exit_signals(signum, frame):
     publish_event(EVENT_PROCESS_TEARDOWN)
     signal.signal(signum, signal.SIG_DFL)
     os.kill(os.getpid(), signum) # Rethrow signal
 
 atexit.register(publish_event, EVENT_PROCESS_TEARDOWN)
-signal.signal(signal.SIGTERM, handle_sigterm)
+signal.signal(signal.SIGTERM, handle_exit_signals)
+signal.signal(signal.SIGINT, handle_exit_signals)
