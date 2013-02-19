3da5b53fdb2fb8c440fa77a861abdb7f1d846162
Tao Wen <taowen@gmail.com>
2013-02-19 11:33:46 +0800

    remove job context manager as it is not being used

--- src/veil/backend/queue/job.py
+++ src/veil/backend/queue/job.py
@@ -1,21 +1,11 @@
 from __future__ import unicode_literals, print_function, division
 from datetime import datetime
 from inspect import isfunction
-import contextlib
 from logging import getLogger
 from veil.utility.clock import *
 from veil_component import *
 
 LOGGER = getLogger(__name__)
-context_managers = []
-
-
-def register_job_context_manager(context_manager):
-    context_managers.append(context_manager)
-    LOGGER.info('registered job context manager: %(context_manager)s', {
-        'context_manager': context_manager
-    })
-
 
 def job(queue, retry_every=None, retry_timeout=0):
     if isfunction(queue):
@@ -42,33 +32,18 @@ class JobHandlerDecorator(object):
 
 
 def perform(job_handler, payload):
-    with nest_context_managers(*context_managers):
-        # restore datetime as utc timezone
-        for key, value in payload.items():
-            if isinstance(value, datetime):
-                payload[key] = convert_datetime_to_utc_timezone(value)
-        try:
-            return job_handler(**payload)
-        except InvalidJob:
-            LOGGER.warn('Invalid job: %(job_handler_name)s, %(payload)s', {
-                'job_handler_name': job_handler.__name__,
-                'payload': payload
-            }, exc_info=1)
-            return
-
-
-def nest_context_managers(*context_managers):
-    context_managers = [as_context_manager(obj) for obj in context_managers]
-    return contextlib.nested(*context_managers)
-
-
-def as_context_manager(obj):
-    if isfunction(obj):
-        return obj()
-    if hasattr(obj, '__enter__') and hasattr(obj, '__exit__'):
-        return obj
-    raise Exception('{} is not context manager'.format(obj))
-
+    # restore datetime as utc timezone
+    for key, value in payload.items():
+        if isinstance(value, datetime):
+            payload[key] = convert_datetime_to_utc_timezone(value)
+    try:
+        return job_handler(**payload)
+    except InvalidJob:
+        LOGGER.warn('Invalid job: %(job_handler_name)s, %(payload)s', {
+            'job_handler_name': job_handler.__name__,
+            'payload': payload
+        }, exc_info=1)
+        return
 
 class InvalidJob(Exception):
     pass
