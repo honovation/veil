3a04666daad8f173049b3f7873638e96b2785429
Tao Wen <taowen@gmail.com>
2013-02-28 11:20:18 +0800

    support --download-only; fix db2 driver upgrade mode

--- src/veil/server/python/python_package_installer.py
+++ src/veil/server/python/python_package_installer.py
@@ -4,19 +4,19 @@ import os
 import re
 from veil_installer import *
 from veil.utility.shell import *
+from veil_component import *
 
 LOGGER = logging.getLogger(__name__)
 PIP_FREEZE_OUTPUT = None
 
 @atomic_installer
 def python_package_resource(name, url=None, **kwargs):
-    pip_package = name
-    installed_version = get_python_package_installed_version(pip_package)
+    installed_version = get_python_package_installed_version(name)
     action = None if installed_version else 'INSTALL'
     if UPGRADE_MODE_LATEST == get_upgrade_mode():
         action = 'UPGRADE'
     elif UPGRADE_MODE_FAST == get_upgrade_mode():
-        latest_version = get_resource_latest_version(to_resource_key(pip_package))
+        latest_version = get_resource_latest_version(to_resource_key(name))
         action = None if latest_version == installed_version else 'UPGRADE'
     elif UPGRADE_MODE_NO == get_upgrade_mode():
         pass
@@ -24,20 +24,33 @@ def python_package_resource(name, url=None, **kwargs):
         raise NotImplementedError()
     dry_run_result = get_dry_run_result()
     if dry_run_result is not None:
-        dry_run_result['python_package?{}'.format(pip_package)] = action or '-'
+        dry_run_result['python_package?{}'.format(name)] = action or '-'
         return
     if not action:
         return
-    LOGGER.info('installing python package: %(pip_package)s ...', {'pip_package': pip_package})
+    LOGGER.info('installing python package: %(name)s ...', {'name': name})
     pip_arg = ''
     mirror = os.getenv('VEIL_DEPENDENCY_MIRROR')
     if mirror:
         pip_arg = '--no-index -f {}:8080'.format(mirror)
     if 'UPGRADE' == action:
         pip_arg = '{} --upgrade'.format(pip_arg)
-    shell_execute('pip install {} {}'.format(url if url else pip_package, pip_arg), capture=True, **kwargs)
-    package_version = get_python_package_installed_version(pip_package, from_cache=False)
-    set_resource_latest_version(to_resource_key(pip_package), package_version)
+    shell_execute('pip install {} {}'.format(url if url else name, pip_arg), capture=True, **kwargs)
+    package_version = get_python_package_installed_version(name, from_cache=False)
+    set_resource_latest_version(to_resource_key(name), package_version)
+
+
+def download_python_package(name, version):
+    install_pip2pi()
+    OPT_PYPI_SIMPLE = as_path('/opt/pypi/simple')
+    for path in (OPT_PYPI_SIMPLE / name).files():
+        if path.startswith('{}-{}'.format(name, version))
+    shell_execute('pip2pi /opt/pypi {}'.format(name))
+
+
+def install_pip2pi():
+    if not get_python_package_installed_version('pip2pi'):
+        shell_execute('pip install pip2pi', capture=True)
 
 
 def to_resource_key(pip_package):
