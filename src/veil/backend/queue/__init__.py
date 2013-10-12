62400f2cf6a271a31e1c5234070450b1ccb136d7
Eric Du <eric@honovation.com>
2013-10-12 23:57:18 +0800

    expose delete_pending_jobs & delete_failed_jobs

--- src/veil/backend/queue/__init__.py
+++ src/veil/backend/queue/__init__.py
@@ -5,6 +5,8 @@ with veil_component.init_component(__name__):
     from .job import InvalidJob
     from .periodic_job import periodic_job
     from .queue import register_queue
+    from .queue_maintenance import delete_pending_jobs
+    from .queue_maintenance import delete_failed_jobs
     from .server.resweb_installer import resweb_resource
     from .queue_client_installer import queue_client_resource
 
@@ -13,6 +15,8 @@ with veil_component.init_component(__name__):
         InvalidJob.__name__,
         periodic_job.__name__,
         register_queue.__name__,
+        delete_pending_jobs.__name__,
+        delete_failed_jobs.__name__,
         resweb_resource.__name__,
         queue_client_resource.__name__
     ]
