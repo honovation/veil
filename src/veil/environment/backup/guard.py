1dc8b8765d96344fcc2e7fc2a4a3bb4d0107c7b3
dawncold <loooseleaves@gmail.com>
2013-10-14 17:24:51 +0800

    add TEARDOWN test for guard

--- src/veil/environment/backup/guard.py
+++ src/veil/environment/backup/guard.py
@@ -1,6 +1,10 @@
 from __future__ import unicode_literals, print_function, division
 import logging
+import datetime
 from veil.frontend.cli import *
+from veil.model.event import *
+from veil.server.process import *
+from veil.utility.shell import *
 from veil.utility.timer import *
 from .env_backup import create_env_backup
 
@@ -13,3 +17,8 @@ def bring_up_guard(crontab_expression):
         create_env_backup()
 
     work()
+
+
+@event(EVENT_PROCESS_TEARDOWN)
+def on_process_teardown():
+    shell_execute('touch ~/has_shutdown_at_{}'.format(datetime.datetime.now().strftime('%Y%m%d%H%M%S')))
