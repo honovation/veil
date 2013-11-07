098428e7617c903078b6cdc2296763dd78646f83
Eric Du <eric@honovation.com>
2013-11-07 15:13:57 +0800

    refactor

--- src/veil/server/supervisor/supervisorctl.py
+++ src/veil/server/supervisor/supervisorctl.py
@@ -5,9 +5,9 @@ from veil.utility.shell import *
 
 LOGGER = logging.getLogger(__name__)
 
+
 def supervisorctl(action, *arguments, **kwargs):
-    return shell_execute('veil execute supervisorctl -c {} {} {}'.format(
-        VEIL_ETC_DIR / 'supervisor.cfg', action, ' '.join(arguments)), **kwargs)
+    return shell_execute('veil execute supervisorctl -c {} {} {}'.format(VEIL_ETC_DIR / 'supervisor.cfg', action, ' '.join(arguments)), **kwargs)
 
 
 def is_supervisord_running():
@@ -18,18 +18,15 @@ def is_supervisord_running():
     if not (VEIL_ETC_DIR / 'supervisor.cfg').exists():
         return False
     try:
-        output = shell_execute('veil execute supervisorctl -c {} {}'.format(
-            VEIL_ETC_DIR / 'supervisor.cfg', 'status'), capture=True)
-        return 'refused' not in output
+        output = shell_execute('veil execute supervisorctl -c {} {}'.format(VEIL_ETC_DIR / 'supervisor.cfg', 'status'), capture=True)
     except Exception as e:
         if isinstance(e, ShellExecutionError) and 'SHUTDOWN_STATE' in e.output:
             return True
         LOGGER.exception('Exception occurred while checking if supervisord is running')
         return True
+    else:
+        return 'refused' not in output
 
 
 def are_all_supervisord_programs_running():
-    for line in supervisorctl('status', capture=True).splitlines(False):
-        if line and 'RUNNING' not in line:
-            return False
-    return True
+    return all(not line or 'RUNNING' in line for line in supervisorctl('status', capture=True).splitlines(False))
