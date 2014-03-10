f22c4e6606226aa10cb01a2aade99ea4f9128977
Eric Du <eric@honovation.com>
2014-03-10 15:58:33 +0800

    configure ssl in listen

--- src/veil/frontend/nginx_setting.py
+++ src/veil/frontend/nginx_setting.py
@@ -20,10 +20,10 @@ def nginx_program(servers, enable_compression=False, has_bunker=False, is_bunker
     })
 
 
-def nginx_server(server_name, listen, locations, upstreams=None, error_page=None, error_page_dir=None, default_server=False, **kwargs):
+def nginx_server(server_name, listen, locations, upstreams=None, error_page=None, error_page_dir=None, ssl=False, default_server=False, **kwargs):
     return {
         server_name: dict({
-            'listen': '{}{}'.format(listen, ' default_server' if default_server else ''),
+            'listen': '{}{}{}'.format(listen, ' ssl' if ssl else '', ' default_server' if default_server else ''),
             'locations': locations,
             'upstreams': upstreams,
             'error_page': error_page,
