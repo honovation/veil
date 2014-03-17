c86121d26350031fab842db743c50bf66a6063da
dawncold <loooseleaves@gmail.com>
2014-03-17 16:30:50 +0800

    fix veil init

--- src/veil/server/in_service/local_deployer.py
+++ src/veil/server/in_service/local_deployer.py
@@ -13,8 +13,7 @@ LOGGER = logging.getLogger(__name__)
 def deploy():
     assert_no_local_change(VEIL_FRAMEWORK_HOME)
     assert_no_local_change(VEIL_HOME)
-    shell_execute('veil install veil_installer.component_resource?veil.server.supervisor --upgrade-mode={}'.format(
-        UPGRADE_MODE_FAST))
+    shell_execute('veil install veil_installer.component_resource?veil.server.supervisor --upgrade-mode={}'.format(UPGRADE_MODE_FAST))
     shell_execute('veil down')
     shell_execute('veil install-server --upgrade-mode={}'.format(UPGRADE_MODE_FAST))
     shell_execute('veil up --daemonize')
