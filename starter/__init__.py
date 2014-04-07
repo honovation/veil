a4a689306059ed3a9b9db86745dfa1256f99b213
Eric Du <eric@honovation.com>
2014-04-07 19:51:09 +0800

    move const under vsee

---
+++ starter/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals, print_function, division
+
+
--- starter/vsee/src/const.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-from __future__ import unicode_literals, print_function, division
-
-GENDER_MALE = 1
-GENDER_FEMALE = 2
-GENDER = {
-    GENDER_MALE: '男',
-    GENDER_FEMALE: '女'
-}
