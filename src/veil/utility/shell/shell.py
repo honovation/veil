475f2d45a626b1423d8d9ddf627d681240ddefbb
Eric Du <eric@honovation.com>
2014-05-16 13:49:58 +0800

    remove shell_execute debug

--- src/veil/utility/shell/shell.py
+++ src/veil/utility/shell/shell.py
@@ -8,8 +8,8 @@ LOGGER = logging.getLogger(__name__)
 
 
 def shell_execute(command_line, capture=False, waits=True, shell=True, debug=False, expected_return_codes=(0,), **kwargs):
-    # if debug:
-    LOGGER.debug('shell execute: %(command_line)s', {'command_line': command_line})
+    if debug:
+        LOGGER.debug('shell execute: %(command_line)s', {'command_line': command_line})
     if capture:
         kwargs.update(dict(stderr=subprocess.STDOUT, stdout=subprocess.PIPE, stdin=subprocess.PIPE))
     command_args = command_line if shell else shlex.split(command_line)
@@ -21,12 +21,6 @@ def shell_execute(command_line, capture=False, waits=True, shell=True, debug=Fal
     if not waits:
         return process
     output = process.communicate()[0]
-    LOGGER.debug('shell execute result: %(return_code)s, %(command_line)s, %(kwargs)s, %(output)s', {
-        'return_code': process.returncode,
-        'command_line': command_line,
-        'kwargs': kwargs,
-        'output': output
-    })
     if process.returncode not in expected_return_codes:
         LOGGER.warn('received nonzero return code: %(return_code)s, %(command_line)s, %(kwargs)s, %(output)s', {
             'return_code': process.returncode,
