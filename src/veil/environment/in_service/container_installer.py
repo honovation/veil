f20ba6922776b530d2762e099c68c5e7287d863b
Eric Du <eric@honovation.com>
2014-06-07 08:13:50 +0800

    fix

--- src/veil/environment/in_service/container_installer.py
+++ src/veil/environment/in_service/container_installer.py
@@ -131,7 +131,7 @@ def veil_container_init_resource(server):
 
     fabric.contrib.files.append('{}/etc/ssh/sshd_config'.format(container_rootfs_path),
         ['PasswordAuthentication no', 'GatewayPorts clientspecified', 'MaxSessions 128'], use_sudo=True)
-    if 'precise' == fabric.api.run('chroot {} lsb_release -cs'.format(container_rootfs_path)):
+    if 'precise' == fabric.api.sudo('chroot {} lsb_release -cs'.format(container_rootfs_path)):
         fabric.api.sudo('chroot {} /etc/init.d/ssh restart'.format(container_rootfs_path))
     else:
         fabric.api.sudo('chroot {} service ssh reload'.format(container_rootfs_path))
@@ -158,7 +158,7 @@ def veil_container_sources_list_resource(server):
     sources_list_path = '/etc/apt/sources.list'
     full_sources_list_path = '{}{}'.format(container_rootfs_path, sources_list_path)
     fabric.api.sudo('chroot {} cp -pn {path} {path}.origin'.format(container_rootfs_path, path=sources_list_path))
-    context = dict(mirror=VEIL_APT_URL, codename=fabric.api.run('chroot {} lsb_release -cs'.format(container_rootfs_path)))
+    context = dict(mirror=VEIL_APT_URL, codename=fabric.api.sudo('chroot {} lsb_release -cs'.format(container_rootfs_path)))
     fabric.contrib.files.upload_template('sources.list.j2', full_sources_list_path, context=context, use_jinja=True, template_dir=CURRENT_DIR,
         use_sudo=True, backup=False, mode=0644)
 
