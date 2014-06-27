2674ec6211350e67c543db55bc1c7f5269f43e95
Eric Du <eric@honovation.com>
2014-06-27 18:13:14 +0800

    refactor

--- src/veil/environment/backup/env_backup.py
+++ src/veil/environment/backup/env_backup.py
@@ -31,27 +31,28 @@ def create_env_backup():
     with fabric.api.settings(disable_known_hosts=True, key_filename=SSH_KEY_PATH):
         timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
         for host in hosts_to_backup:
-            backup_host(host, timestamp)
-            fetch_host_backup(host, timestamp)
-        shell_execute('ln -sf {} latest'.format(timestamp), cwd=VEIL_BACKUP_ROOT)
-        delete_old_backups()
-        rsync_to_backup_mirror()
-
-
-def delete_old_backups():
-    shell_execute('find . -type d -ctime +{} -exec rm -r {{}} +'.format(KEEP_BACKUP_FOR_DAYS), cwd=VEIL_BACKUP_ROOT)
+            with fabric.api.settings(host_string=host.deploys_via):
+                backup_host(host, timestamp)
+                fetch_host_backup(host, timestamp)
+    shell_execute('ln -sf {} latest'.format(timestamp), cwd=VEIL_BACKUP_ROOT)
+    delete_old_backups()
+    rsync_to_backup_mirror()
 
 
 def backup_host(host, timestamp):
     host_backup_dir = host.ssh_user_home / 'tmp' / VEIL_BACKUP_ROOT[1:] / timestamp
     backup_file_template = '{}_{}_{{}}_{}.tar.gz'.format(host.env_name, host.base_name, timestamp)
-    with fabric.api.settings(host_string=host.deploys_via):
-        fabric.api.run('mkdir -p {}'.format(host_backup_dir))
-        with fabric.api.cd(VEIL_VAR_DIR):
-            servers_to_down = [s for s in list_veil_servers(VEIL_ENV_NAME) if s.mount_data_dir and s.host_base_name == host.base_name and is_server_running(s)]
-            for subdir_name in fabric.api.run('find . -maxdepth 1 -mindepth 1 -type d -printf "%P\n"', warn_only=True).splitlines():
-                subdir_backup_path = host_backup_dir / backup_file_template.format(subdir_name)
-                backup_host_dir(subdir_name, subdir_backup_path, servers_to_down)
+    fabric.api.run('mkdir -p {}'.format(host_backup_dir))
+    with fabric.api.cd(VEIL_VAR_DIR):
+        servers_to_down = [s for s in list_veil_servers(VEIL_ENV_NAME) if s.mount_data_dir and s.host_base_name == host.base_name and is_server_running(s)]
+        for subdir_name in fabric.api.run('find . -maxdepth 1 -mindepth 1 -type d -printf "%P\n"', warn_only=True).splitlines():
+            subdir_backup_path = host_backup_dir / backup_file_template.format(subdir_name)
+            backup_host_dir(subdir_name, subdir_backup_path, servers_to_down)
+
+
+def is_server_running(server):
+    ret = fabric.api.run('ps -ef | grep supervisord | grep -e {}/{} | grep -v grep'.format(VEIL_ETC_DIR.parent, server.name), warn_only=True)
+    return ret.return_code == 0
 
 
 @log_elapsed_time
@@ -79,11 +80,6 @@ def bring_up_servers(servers):
                 fabric.api.sudo('veil :{} up --daemonize'.format(server.fullname))
 
 
-def is_server_running(server):
-    ret = fabric.api.run('ps -ef | grep supervisord | grep -e {}/{} | grep -v grep'.format(VEIL_ETC_DIR.parent, server.name), warn_only=True)
-    return ret.return_code == 0
-
-
 def fetch_host_backup(host, timestamp):
     host_backup_dir = host.ssh_user_home / 'tmp' / VEIL_BACKUP_ROOT[1:] / timestamp
     server_guard = get_veil_server(VEIL_ENV_NAME, '@guard')
@@ -94,9 +90,12 @@ def fetch_host_backup(host, timestamp):
     else:
         backup_dir = VEIL_BACKUP_ROOT / timestamp
         backup_dir.makedirs(0755)
-        with fabric.api.settings(host_string=host.deploys_via):
-            fabric.api.get(host_backup_dir / '*', backup_dir)
-            fabric.api.run('rm -rf {}/*'.format(host_backup_dir.parent))
+        fabric.api.get(host_backup_dir / '*', backup_dir)
+        fabric.api.run('rm -rf {}/*'.format(host_backup_dir.parent))
+
+
+def delete_old_backups():
+    shell_execute('find . -type d -ctime +{} -exec rm -r {{}} +'.format(KEEP_BACKUP_FOR_DAYS), cwd=VEIL_BACKUP_ROOT)
 
 
 def rsync_to_backup_mirror():
