f4cfc03b5920a20ead7c13d25b4e990005ed7834
Eric Du <eric@honovation.com>
2014-07-01 09:45:23 +0800

    VEIL_ENV_DIR: use VEIL_ENV_NAME

--- src/veil_component/environment.py
+++ src/veil_component/environment.py
@@ -18,8 +18,8 @@ if '/' in VEIL_SERVER_NAME:
     VEIL_ENV_NAME, VEIL_SERVER_NAME = VEIL_SERVER_NAME.rsplit('/', 1)
 else:
     VEIL_ENV_NAME, VEIL_SERVER_NAME = VEIL_SERVER_NAME, '@'
-VEIL_ENV_SHORT_NAME = VEIL_ENV_NAME.rsplit('--', 1)[0]
-VEIL_ENV_TYPE = VEIL_ENV_SHORT_NAME.rsplit('-', 1)[-1]  # development, test, staging, public (i.e. production)
+VEIL_ENV_BASE_NAME = VEIL_ENV_NAME.rsplit('--', 1)[0]
+VEIL_ENV_TYPE = VEIL_ENV_BASE_NAME.rsplit('-', 1)[-1]  # development, test, staging, public (i.e. production)
 
 CURRENT_OS = namedtuple('VeilOS', 'distname, version, codename')(*platform.linux_distribution())
 # TODO: update after every dev upgrades to trusty
