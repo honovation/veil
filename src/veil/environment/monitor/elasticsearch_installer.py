65ec73dc088a403f86e643d1c84d77e7571a89de
dawncold <loooseleaves@gmail.com>
2014-07-04 23:07:01 +0800

    fixed os package resource usage

--- src/veil/environment/monitor/elasticsearch_installer.py
+++ src/veil/environment/monitor/elasticsearch_installer.py
@@ -12,8 +12,8 @@ def elasticsearch_resource():
     resources = list(BASIC_LAYOUT_RESOURCES)
     resources.extend([
         file_resource(path=APT_SOURCES_LIST_DIR / 'elasticsearch.list', content=render_config('elasticsearch.list')),
-        os_package_resource('elasticsearch=1.1.1'),
-        os_service_resource('elasticsearch', 'not_installed')
+        os_package_resource(name='elasticsearch=1.1.1'),
+        os_service_resource(name='elasticsearch', state='not_installed')
     ])
 
     return resources
