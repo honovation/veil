629eeaea3dc8dc0168a46f4eab61d8019ccb95d0
dawncold <loooseleaves@gmail.com>
2014-07-04 23:56:19 +0800

    fix opt dir

--- src/veil/environment/monitor/elasticsearch_installer.py
+++ src/veil/environment/monitor/elasticsearch_installer.py
@@ -7,8 +7,8 @@ from veil.profile.installer import *
 @composite_installer
 def elasticsearch_resource():
     if not (OPT_DIR / 'elasticsearch-1.1.1.tar.gz').exists():
-        shell_execute('wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.1.tar.gz')
+        shell_execute('wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.1.tar.gz', cwd=OPT_DIR)
     if not (OPT_DIR / 'elasticsearch-1.1.1').exists():
-        shell_execute('tar zxf elasticsearch-1.1.1.tar.gz')
+        shell_execute('tar zxf elasticsearch-1.1.1.tar.gz', cwd=OPT_DIR)
 
     return list(BASIC_LAYOUT_RESOURCES)
