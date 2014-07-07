69e9800f158365ffe53db5143038272bf227e0ac
dawncold <loooseleaves@gmail.com>
2014-07-07 17:00:30 +0800

    unzip is pre-installed for every host and container

--- src/veil/environment/monitor/kibana_installer.py
+++ src/veil/environment/monitor/kibana_installer.py
@@ -13,16 +13,15 @@ def kibana_resource(config):
     if not (OPT_DIR / 'kibana-latest.zip').exists():
         shell_execute('wget http://download.elasticsearch.org/kibana/kibana/kibana-latest.zip', cwd=OPT_DIR)
     if not KIBANA_DIR.exists():
-        shell_execute('apt-get install unzip')
         shell_execute('unzip kibana-latest.zip', cwd=OPT_DIR)
         if (KIBANA_DIR / 'app/dashboards/logstash.json').exists():
             shell_execute('mv {} default.json'.format(KIBANA_DIR / 'app/dashboards/logstash.json'))
     resources = [
-        file_resource(path=KIBANA_DIR / 'config.js', content=render_config('kibana.config.js.j2', **config)),
         os_ppa_repository_resource(name='nginx/stable'),
         os_package_resource(name='nginx-extras'),
         os_service_resource(state='not_installed', name='nginx'),
-        file_resource(path=VEIL_ETC_DIR / 'nginx.conf', content=render_config('nginx.conf.j2', kibana_root=KIBANA_DIR))
+        file_resource(path=VEIL_ETC_DIR / 'nginx.conf', content=render_config('nginx.conf.j2', kibana_root=KIBANA_DIR)),
+        file_resource(path=KIBANA_DIR / 'config.js', content=render_config('kibana.config.js.j2', **config))
     ]
 
     return resources
