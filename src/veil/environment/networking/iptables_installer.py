0835454317fb30fe3ff6231053d120fa3fc10bf6
dawncold <loooseleaves@gmail.com>
2014-11-14 11:12:32 +0800

    iptables installer use fabric

--- src/veil/environment/networking/iptables_installer.py
+++ src/veil/environment/networking/iptables_installer.py
@@ -1,14 +1,14 @@
 from __future__ import unicode_literals, print_function, division
 import logging
+import fabric.api
 from veil_installer import *
-from veil.utility.shell import *
 
 LOGGER = logging.getLogger(__name__)
 
 
 @atomic_installer
 def iptables_rule_resource(table, rule):
-    installed = rule in shell_execute('sudo iptables-save -t {}'.format(table), capture=True)
+    installed = rule in fabric.api.sudo('iptables-save -t {}'.format(table), warn_only=True)
     dry_run_result = get_dry_run_result()
     if dry_run_result is not None:
         dry_run_result['iptables?-t {} -A {}'.format(table, rule)] = '-' if installed else 'EXECUTE'
@@ -16,8 +16,8 @@ def iptables_rule_resource(table, rule):
     if installed:
         return
     LOGGER.info('install iptables rule: %(rule)s to table %(table)s...', {'rule': rule, 'table': table})
-    shell_execute('sudo iptables -t {} -A {}'.format(table, rule))
-    updated_rules = shell_execute('sudo iptables-save -t {}'.format(table), capture=True)
+    fabric.api.sudo('iptables -t {} -A {}'.format(table, rule), warn_only=True)
+    updated_rules = fabric.api.sudo('sudo iptables-save -t {}'.format(table), warn_only=True)
     if rule not in updated_rules:
         raise Exception(
             'rule doest not match the exact format of iptables-save, which will cause duplicated entry in the future, please FIX IT.\n{}'.format(
@@ -26,19 +26,19 @@ def iptables_rule_resource(table, rule):
 
 @atomic_installer
 def iptables_rule_removed_resource(table, rule):
-    installed = rule in shell_execute('sudo iptables-save -t {}'.format(table), capture=True)
+    installed = rule in fabric.api.sudo('iptables-save -t {}'.format(table), warn_only=True)
     dry_run_result = get_dry_run_result()
     if dry_run_result is not None:
         dry_run_result['iptables?-t {} -D {}'.format(table, rule)] = 'EXECUTE' if installed else ''
         return
     if installed:
         LOGGER.info('remove iptables rule: %(rule)s from table %(table)s...', {'rule': rule, 'table': table})
-        shell_execute('sudo iptables -t {} -D {}'.format(table, rule))
+        fabric.api.sudo('iptables -t {} -D {}'.format(table, rule), warn_only=True)
 
 
 @atomic_installer
 def iptables_policy_resource(table, chain, policy):
-    installed = '{} {}'.format(chain, policy) in shell_execute('sudo iptables-save -t {}'.format(table), capture=True)
+    installed = '{} {}'.format(chain, policy) in fabric.api.sudo('iptables-save -t {}'.format(table), warn_only=True)
     dry_run_result = get_dry_run_result()
     if dry_run_result is not None:
         dry_run_result['iptables?-t {} -P {} {}'.format(table, chain, policy)] = '-' if installed else 'EXECUTE'
@@ -47,7 +47,7 @@ def iptables_policy_resource(table, chain, policy):
         return
     LOGGER.info('install iptables policy: %(chain)s %(policy)s to table %(table)s...', {
         'chain': chain, 'policy': policy, 'table': table})
-    shell_execute('sudo iptables -t {} -P {} {}'.format(table, chain, policy))
+    fabric.api.sudo('iptables -t {} -P {} {}'.format(table, chain, policy), warn_only=True)
 
 
 def list_iptables_resources_to_secure_host():
