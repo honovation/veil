775aa99bec6b08b3941c7a06bdd47607b701210c
Eric Du <eric@honovation.com>
2014-11-27 11:56:42 +0800

    memoize: max_age

--- src/veil/utility/memoize/memoize.py
+++ src/veil/utility/memoize/memoize.py
@@ -3,17 +3,18 @@ from __future__ import unicode_literals, print_function, division
 import functools
 import inspect
 import weakref
+import time
 
 
-def memoize(max_size=None):
+def memoize(max_size=None, max_age=None):
     if hasattr(max_size, '__call__'):
         func = max_size
         decorator = _memoize()
         return decorator(func)
-    return _memoize(max_size)
+    return _memoize(max_size, max_age)
 
 
-def _memoize(max_size=None):
+def _memoize(max_size=None, max_age=None):
     class Memoizer(object):
         """
         Decorator. Caches a function's return value each time it is called.
@@ -26,6 +27,7 @@ def _memoize(max_size=None):
         def __init__(self, func):
             self.func = func
             self.max_size = max_size
+            self.max_age = max_age
             self.cache = {}
             self.size = 0
             Memoizer.caches.add(self)
@@ -33,11 +35,22 @@ def _memoize(max_size=None):
         def __call__(self, *args, **kwargs):
             key = self.key(args, kwargs)
             try:
-                return self.cache[key]
+                if self.max_age:
+                    now = time.time()
+                    value, cached_at = self.cache[key]
+                    age = now - cached_at
+                    if age > self.max_age:
+                        raise KeyError('expired')
+                else:
+                    value = self.cache[key]
+                return value
             except KeyError:
                 value = self.func(*args, **kwargs)
                 if self.max_size is None or self.size < self.max_size:
-                    self.cache[key] = value
+                    if self.max_age:
+                        self.cache[key] = (value, now)
+                    else:
+                        self.cache[key] = value
                     self.size += 1
                 return value
             except TypeError:
