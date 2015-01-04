ae2e4527849e72b1c7c8f34bf937dcd5785411bf
Eric Du <eric@honovation.com>
2015-01-04 16:24:44 +0800

    add veil.model.event, veil.backend.redis and veil.backend.queue into veil.profile.model

--- src/veil/profile/model/__init__.py
+++ src/veil/profile/model/__init__.py
@@ -4,6 +4,9 @@ with veil_component.init_component(__name__):
     from veil.model.collection import *
     from veil.model.binding import *
     from veil.model.command import *
+    from veil.model.event import *
     from veil.model.security import *
     from veil.utility.clock import *
     from veil.backend.database.client import *
+    from veil.backend.redis import *
+    from veil.backend.queue import *
