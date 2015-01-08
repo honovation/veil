63a67ae5892e09ba6a079308739fdde8bc286938
dawncold <loooseleaves@gmail.com>
2015-01-08 09:03:03 +0800

    environment_variables is dict

--- src/veil/environment/monitor_setting.py
+++ src/veil/environment/monitor_setting.py
@@ -9,13 +9,13 @@ def monitor_programs(config):
         redis_program('log_buffer', config.log_buffer_redis_host, config.log_buffer_redis_port, snapshot_configs=[
             DictObject(interval=interval, changed_keys=changed_keys) for interval, changed_keys in [(900, 1), (300, 10), (60, 10000)]]),
         {'logstash': {
-            'environment_variables': 'LS_HEAP_SIZE={}'.format(config.ls_heap_size),
+            'environment_variables': {'LS_HEAP_SIZE': config.ls_heap_size},
             'execute_command': '/opt/logstash/bin/logstash agent -f {}/logstash.conf -v'.format(VEIL_ETC_DIR),
             'run_as': 'root',
             'resources': [('veil.environment.monitor.logstash_resource', {'config': config})]
         }},
         {'elasticsearch': {
-            'environment_variables': 'ES_HEAP_SIZE={}'.format(config.es_heap_size),
+            'environment_variables': {'ES_HEAP_SIZE': config.es_heap_size},
             'execute_command': '/usr/share/elasticsearch/bin/elasticsearch -Des.path.conf={}'.format(VEIL_ETC_DIR),
             'run_as': 'root',
             'resources': [('veil.environment.monitor.elasticsearch_resource', {'config': config})]
