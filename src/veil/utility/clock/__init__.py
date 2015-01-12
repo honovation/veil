c47b47cb6f0974663b244fe9add9e3c14bf9f7cd
Eric Du <eric@honovation.com>
2015-01-12 13:01:20 +0800

    expose convert_datetime_to_timestamp

--- src/veil/utility/clock/__init__.py
+++ src/veil/utility/clock/__init__.py
@@ -10,6 +10,7 @@ with veil_component.init_component(__name__):
     from .clock import convert_datetime_to_naive_local
     from .clock import convert_datetime_to_client_timezone
     from .clock import convert_datetime_to_utc_timezone
+    from .clock import convert_datetime_to_timestamp
     from .clock import is_naive_datetime
     from .clock import convert_naive_datetime_to_aware
     from .clock import get_relative_delta
@@ -25,6 +26,7 @@ with veil_component.init_component(__name__):
         convert_datetime_to_naive_local.__name__,
         convert_datetime_to_client_timezone.__name__,
         convert_datetime_to_utc_timezone.__name__,
+        convert_datetime_to_timestamp.__name__,
         is_naive_datetime.__name__,
         convert_naive_datetime_to_aware.__name__,
         get_relative_delta.__name__,
