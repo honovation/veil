2ab9efa19b8044107867581b115168339c75ce7d
Eric Du <eric@honovation.com>
2015-03-13 13:32:09 +0800

    clean code

--- src/veil/development/test/case.py
+++ src/veil/development/test/case.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals, print_function, division
 import unittest.case
 
-executing_test = None
-bootstrappers = []
+_executing_test = None
+_bootstrappers = []
+
 
 def register_test_hook(bootstrapper):
-    bootstrappers.append(bootstrapper)
+    _bootstrappers.append(bootstrapper)
 
 
 def test_hook(func):
@@ -14,19 +15,18 @@ def test_hook(func):
 
 
 def set_executing_test(test):
-    global executing_test
-    executing_test = test
+    global _executing_test
+    _executing_test = test
 
 
 def get_executing_test(optional=False):
-    if executing_test is None and not optional:
+    if _executing_test is None and not optional:
         raise Exception('no test executing')
-    return executing_test
+    return _executing_test
 
 
 def set_up_fake_test():
-    global executing_test
-    if executing_test:
+    if _executing_test:
         raise Exception('test is executing')
     else:
         FakeTestCase().setUp()
@@ -37,12 +37,13 @@ def tear_down_fake_test():
     if executing_test:
         executing_test.doCleanups()
 
+
 class TestCase(unittest.case.TestCase):
     def setUp(self):
         super(TestCase, self).setUp()
         set_executing_test(self)
         self.addCleanup(lambda: set_executing_test(None))
-        for bootstrapper in bootstrappers:
+        for bootstrapper in _bootstrappers:
             bootstrapper()
 
 
