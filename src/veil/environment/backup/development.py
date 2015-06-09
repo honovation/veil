9f2928ffb9d45253606e2a90bec0a0e8ec42d465
Eric Du <eric@honovation.com>
2015-06-09 11:34:50 +0800

    download baseline from backup mirror

--- src/veil/environment/backup/development.py
+++ src/veil/environment/backup/development.py
@@ -15,8 +15,9 @@ BASELINE_DIR = VEIL_HOME / 'baseline'
 def restore_from_baseline(veil_env_name, force_download='FALSE', relative_path=None, host_name=None):
     """
     Examples:
-        sudo veil restore-from-baseline xxx-public TRUE data/xxx-postgresql-9.3
-        sudo veil restore-from-baseline xxx-public FALSE data/xxx-postgresql-9.3
+        sudo -E veil :ljmall-staging restore-from-baseline xxx-public TRUE data/xxx-postgresql-9.3
+        sudo -E veil restore-from-baseline xxx-public TRUE data/xxx-postgresql-9.3
+        sudo -E veil restore-from-baseline xxx-public FALSE data/xxx-postgresql-9.3
     """
     if not host_name:
         for server in list_veil_servers(veil_env_name):
@@ -28,11 +29,11 @@ def restore_from_baseline(veil_env_name, force_download='FALSE', relative_path=N
         assert any(server.mount_data_dir for server in host.server_list), 'Please specify relative_path'
         relative_path = host.var_dir.relpathto(host.data_dir)
     if relative_path == '*':
-        remote_path = VEIL_BACKUP_ROOT / 'latest' / host.base_name
+        remote_path = as_path('latest') / host.base_name
         baseline_path = BASELINE_DIR / veil_env_name / host.base_name
         restored_to_path = VEIL_VAR_DIR
     else:
-        remote_path = VEIL_BACKUP_ROOT / 'latest' / host.base_name / relative_path
+        remote_path = as_path('latest') / host.base_name / relative_path
         baseline_path = BASELINE_DIR / veil_env_name / host.base_name / relative_path
         restored_to_path = VEIL_VAR_DIR / relative_path
     if force_download.upper() == 'TRUE' or not baseline_path.exists():
@@ -69,13 +70,15 @@ def restore_from_baseline(veil_env_name, force_download='FALSE', relative_path=N
 @script('download-baseline')
 @log_elapsed_time
 def download_baseline(veil_env_name, remote_path, baseline_path):
-    #TODO: download from BACKUP MIRROR
     if isinstance(baseline_path, basestring):
         baseline_path = as_path(baseline_path)
     baseline_path.makedirs(0755)
-    guard_key = as_path('../ljsecurity') / veil_env_name / '.ssh-@guard' / 'id_rsa'
-    server_guard = get_veil_server(veil_env_name, '@guard')
-    host_guard = get_veil_host(veil_env_name, server_guard.host_name)
-    container_rootfs_path = '/var/lib/lxc/{}/rootfs'.format(server_guard.container_name)
-    shell_execute('''rsync -avhPz -e "ssh -i {} -p {} -T -x -o Compression=no -o StrictHostKeyChecking=no" --delete root@{}:{}/{}/ {}/'''.format(
-        guard_key, host_guard.ssh_port, host_guard.internal_ip, container_rootfs_path, remote_path, baseline_path), debug=True)
+    backup_mirror = get_veil_server(veil_env_name, '@guard').backup_mirror
+    if not backup_mirror:
+        raise Exception('backup mirror not found on server {}/{}'.format(veil_env_name, '@guard'))
+    if not hasattr(backup_mirror, 'domain'):
+        backup_mirror.domain = 'ljhost-01.dmright.com'
+    backup_mirror_path = '~/backup_mirror/{}'.format(veil_env_name)
+    shell_execute('''rsync -avhPz -e "ssh -p {} -T -x -o Compression=yes -o StrictHostKeyChecking=no" --delete --bwlimit={} {}@{}:{}/{}/ {}/'''.format(
+        backup_mirror.ssh_port, backup_mirror.bandwidth_limit, backup_mirror.ssh_user, backup_mirror.domain, backup_mirror_path, remote_path,
+        baseline_path), debug=True)
