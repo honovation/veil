fe72cdabf9b8209f995a2c6310f4dfcda5c987e3
Eric Du <eric@honovation.com>
2015-06-15 16:59:24 +0800

    objectify/entitify/freeze_dict_object/unfreeze_dict_object: one object for multiple references

--- src/veil/model/collection/collection.py
+++ src/veil/model/collection/collection.py
@@ -1,66 +1,107 @@
 from __future__ import unicode_literals, print_function, division
 
 DEFAULT_PRIMARY_KEYS = ('id',)
+id2obj = {}
 
 
 def objectify(o):
-    if isinstance(o, Entity):
-        return o
-    if isinstance(o, DictObject):
-        primary_keys = o.pop('primary_keys', None)
-        return Entity(o, primary_keys=primary_keys) if primary_keys else o
-    elif isinstance(o, dict):
-        primary_keys = o.pop('primary_keys', None)
-        if primary_keys:
-            return Entity(((k, objectify(v)) for k, v in o.items()), primary_keys=primary_keys)
+    try:
+        return _objectify(o)
+    finally:
+        id2obj.clear()
+
+
+def _objectify(o):
+    id_ = id(o)
+    if id_ not in id2obj:
+        if isinstance(o, Entity):
+            id2obj[id_] = o
+        elif isinstance(o, DictObject):
+            primary_keys = o.pop('primary_keys', None)
+            id2obj[id_] = Entity(o, primary_keys=primary_keys) if primary_keys else o
+        elif isinstance(o, dict):
+            primary_keys = o.pop('primary_keys', None)
+            if primary_keys:
+                id2obj[id_] = Entity(((k, _objectify(v)) for k, v in o.items()), primary_keys=primary_keys)
+            else:
+                id2obj[id_] = DictObject((k, _objectify(v)) for k, v in o.items())
+        elif isinstance(o, (tuple, set, list)):
+            id2obj[id_] = o.__class__(_objectify(e) for e in o)
         else:
-            return DictObject((k, objectify(v)) for k, v in o.items())
-    elif isinstance(o, (tuple, set, list)):
-        return o.__class__(objectify(e) for e in o)
-    else:
-        return o
+            id2obj[id_] = o
+    return id2obj[id_]
 
 
 def entitify(o, primary_keys=True):
-    if isinstance(o, Entity):
-        return o
-    elif isinstance(o, (DictObject, dict)):
-        primary_keys_ = o.pop('primary_keys', primary_keys)
-        return Entity(((k, entitify(v, primary_keys=primary_keys)) for k, v in o.items()), primary_keys=primary_keys_)
-    elif isinstance(o, (tuple, set, list)):
-        return o.__class__(entitify(e, primary_keys=primary_keys) for e in o)
-    else:
-        return o
+    try:
+        return _entitify(o, primary_keys)
+    finally:
+        id2obj.clear()
+
+
+def _entitify(o, primary_keys):
+    id_ = id(o)
+    if id_ not in id2obj:
+        if isinstance(o, Entity):
+            id2obj[id_] = o
+        elif isinstance(o, (DictObject, dict)):
+            primary_keys_ = o.pop('primary_keys', primary_keys)
+            id2obj[id_] = Entity(((k, _entitify(v, primary_keys)) for k, v in o.items()), primary_keys=primary_keys_)
+        elif isinstance(o, (tuple, set, list)):
+            id2obj[id_] = o.__class__(_entitify(e, primary_keys) for e in o)
+        else:
+            id2obj[id_] = o
+    return id2obj[id_]
 
 
 def freeze_dict_object(o):
-    if isinstance(o, (FrozenDictObject, FrozenEntity)):
-        return o
-    elif isinstance(o, Entity):
-        return FrozenEntity((k, freeze_dict_object(v)) for k, v in o.items())
-    elif isinstance(o, DictObject) or type(o) is dict:
-        return FrozenDictObject((k, freeze_dict_object(v)) for k, v in o.items())
-    elif isinstance(o, dict):
-        return o.__class__((k, freeze_dict_object(v)) for k, v in o.items())
-    elif isinstance(o, (tuple, set, list)):
-        return o.__class__(freeze_dict_object(e) for e in o)
-    else:
-        return o
+    try:
+        return _freeze_dict_object(o)
+    finally:
+        id2obj.clear()
+
+
+def _freeze_dict_object(o):
+    id_ = id(o)
+    if id_ not in id2obj:
+        if isinstance(o, (FrozenDictObject, FrozenEntity)):
+            id2obj[id_] = o
+        elif isinstance(o, Entity):
+            id2obj[id_] = FrozenEntity((k, _freeze_dict_object(v)) for k, v in o.items())
+        elif isinstance(o, DictObject) or type(o) is dict:
+            id2obj[id_] = FrozenDictObject((k, _freeze_dict_object(v)) for k, v in o.items())
+        elif isinstance(o, dict):
+            id2obj[id_] = o.__class__((k, _freeze_dict_object(v)) for k, v in o.items())
+        elif isinstance(o, (tuple, set, list)):
+            id2obj[id_] = o.__class__(_freeze_dict_object(e) for e in o)
+        else:
+            id2obj[id_] = o
+    return id2obj[id_]
 
 
 def unfreeze_dict_object(o):
-    if isinstance(o, FrozenEntity):
-        return Entity((k, unfreeze_dict_object(v)) for k, v in o.items())
-    elif isinstance(o, FrozenDictObject) or type(o) is dict:
-        return DictObject((k, unfreeze_dict_object(v)) for k, v in o.items())
-    elif isinstance(o, DictObject):
-        return o
-    elif isinstance(o, dict):
-        return o.__class__((k, unfreeze_dict_object(v)) for k, v in o.items())
-    elif isinstance(o, (tuple, set, list)):
-        return o.__class__(unfreeze_dict_object(e) for e in o)
-    else:
-        return o
+    try:
+        return _unfreeze_dict_object(o)
+    finally:
+        id2obj.clear()
+
+
+def _unfreeze_dict_object(o):
+    id_ = id(o)
+    if id_ not in id2obj:
+        if isinstance(o, FrozenEntity):
+            id2obj[id_] = Entity((k, _unfreeze_dict_object(v)) for k, v in o.items())
+        elif isinstance(o, FrozenDictObject) or type(o) is dict:
+            id2obj[id_] = DictObject((k, _unfreeze_dict_object(v)) for k, v in o.items())
+        elif isinstance(o, DictObject):
+            id2obj[id_] = o
+        elif isinstance(o, dict):
+            id2obj[id_] = o.__class__((k, _unfreeze_dict_object(v)) for k, v in o.items())
+        elif isinstance(o, (tuple, set, list)):
+            id2obj[id_] = o.__class__(_unfreeze_dict_object(e) for e in o)
+        else:
+            id2obj[id_] = o
+    return id2obj[id_]
 
 
 class DictObject(dict):
