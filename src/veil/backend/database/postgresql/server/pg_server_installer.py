bdb2a8ef8100222b83af0d22a950f466e2fcd097
Eric Du <eric@honovation.com>
2015-06-23 19:29:59 +0800

    remove todo after upgrading PG to 9.4

--- src/veil/backend/database/postgresql/server/pg_server_installer.py
+++ src/veil/backend/database/postgresql/server/pg_server_installer.py
@@ -25,8 +25,7 @@ def postgresql_server_resource(purpose, config):
     resources = list(BASIC_LAYOUT_RESOURCES)
     resources.extend([
         postgresql_apt_repository_resource(),
-        #TODO: remove cmd_run_before_install at below at next PostgreSQL server upgrade 20150313
-        os_package_resource(name='postgresql-{}'.format(config.version), cmd_run_before_install='rm -f /etc/sysctl.d/30-postgresql-shm.conf')])
+        os_package_resource(name='postgresql-{}'.format(config.version))])
     resources.extend([
         os_service_resource(state='not_installed', name='postgresql'),
         postgresql_cluster_resource(purpose=purpose, version=config.version, owner=config.owner, owner_password=config.owner_password),
