d7d21fb1fade52f3ef9f021545f89659ff889811
dawncold <loooseleaves@gmail.com>
2015-06-28 01:39:22 +0800

    use server fullname

--- src/veil/environment/in_service/container_installer.py
+++ src/veil/environment/in_service/container_installer.py
@@ -53,7 +53,7 @@ def veil_container_lxc_resource(host, server):
     with contextlib.closing(StringIO(installer_file_content)) as f:
         fabric.api.put(f, server.container_installer_path, use_sudo=True, mode=0600)
     with fabric.api.cd(host.veil_home):
-        fabric.api.sudo('veil :{} install veil_installer.installer_resource?{}'.format(host.env_name, server.container_installer_path))
+        fabric.api.sudo('veil :{} install veil_installer.installer_resource?{}'.format(server.fullname, server.container_installer_path))
     fabric.api.sudo('mv -f {} {}'.format(server.container_installer_path, server.installed_container_installer_path))
 
 
