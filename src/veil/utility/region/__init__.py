c29b7cdc3a443641c545ca9ea48b1dc769325fe2
Guo Chen <chenguo@honovation.com>
2015-11-24 13:31:32 +0800

    test parse address: add Initialization data

--- src/veil/utility/region/__init__.py
+++ src/veil/utility/region/__init__.py
@@ -1,7 +1,9 @@
 import veil_component
 
 with veil_component.init_component(__name__):
+    from .region import list_region_name_patterns
     from .region import parse_address
     __all__ = [
+        list_region_name_patterns.__name__,
         parse_address.__name__,
     ]
