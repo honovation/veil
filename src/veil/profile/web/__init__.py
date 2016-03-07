619d22ad55ff1174b57cfcdd8ec08a4dd2ae1e30
Eric Du <eric@honovation.com>
2016-03-07 17:41:20 +0800

    include veil.model.security in veil.profile.web

--- src/veil/profile/web/__init__.py
+++ src/veil/profile/web/__init__.py
@@ -4,5 +4,6 @@ with veil_component.init_component(__name__):
     import httplib
     from veil.frontend.template import *
     from veil.model.command import *
+    from veil.model.security import *
     from veil.frontend.web import *
     from veil.frontend.visitor import *
