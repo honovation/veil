53dd676a3a51e498162bb27a97522b076b1273fa
Eric Du <eric@honovation.com>
2016-03-09 23:55:49 +0800

    add veil.utility.json to veil.profile.model

--- src/veil/profile/model/__init__.py
+++ src/veil/profile/model/__init__.py
@@ -7,6 +7,7 @@ with veil_component.init_component(__name__):
     from veil.model.event import *
     from veil.model.security import *
     from veil.utility.clock import *
+    from veil.utility.json import *
     from veil.backend.database.client import *
     from veil.backend.redis import *
     from veil.backend.queue import *
