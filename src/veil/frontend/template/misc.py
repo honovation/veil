d614d49ffc7c5e05c2c8d851eb96df327a144d44
Eric Du <eric@honovation.com>
2016-03-29 17:26:13 +0800

    feat: introduce custom jinja2 filter quote_plus

--- src/veil/frontend/template/misc.py
+++ src/veil/frontend/template/misc.py
@@ -1,14 +1,21 @@
 from __future__ import unicode_literals, print_function, division
 from veil.utility.json import *
+from veil.utility.http import *
 from veil.environment import *
 from .template import template_filter
 from .template import template_utility
 
 # keep this one minimal, it is global
 
+
 @template_filter
 def json(value):
     return to_json(value)
+
+
+@template_filter('quote_plus')
+def render_quote_plus(value):
+    return quote_plus(value)
 
 
 @template_utility
