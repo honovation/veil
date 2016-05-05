13356a4884ba3460f68ee4c162e1218c39ce5244
TianZhen <tianzhen@honovation.com>
2016-05-05 15:46:20 +0800

    feat: js sdk support jsapi_config

--- src/veil/backend/weixin/js_api_ticket.py
+++ src/veil/backend/weixin/js_api_ticket.py
@@ -16,7 +16,7 @@ WXMP_JSAPI_TICKET_KEY_TPL = 'wxop:{}:wxmp-jsapi-ticket'
 WXMP_JSAPI_TICKET_AUTHORIZATION_URL = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket'
 
 
-def get_wxmp_jsapi_ticket(wxop_app_code, app_id, app_secret, with_ttl=False, jsapi_ticket_to_refresh=None):
+def get_wxmp_jsapi_ticket(wxop_app_code, app_id, app_secret, jsapi_config=None, with_ttl=False, jsapi_ticket_to_refresh=None):
     if VEIL_ENV_TYPE not in {'public', 'development'}:
         raise Exception('cannot get wx access token under environment: {}'.format(VEIL_ENV_TYPE))
     if not jsapi_ticket_to_refresh:
@@ -25,13 +25,17 @@ def get_wxmp_jsapi_ticket(wxop_app_code, app_id, app_secret, with_ttl=False, jsa
             pipe.ttl(WXMP_JSAPI_TICKET_KEY_TPL.format(wxop_app_code))
             jsapi_ticket, ttl = pipe.execute()
     if jsapi_ticket_to_refresh or not jsapi_ticket or ttl <= 0:
-        access_token = get_wxmp_access_token(wxop_app_code, app_id, app_secret)
-        jsapi_ticket, ttl = refresh_wxmp_jsapi_ticket(wxop_app_code, access_token, jsapi_ticket_to_refresh or jsapi_ticket)
+        if jsapi_config:
+            access_token = get_wxmp_access_token(jsapi_config.wxop_app_code, jsapi_config.app_id, jsapi_config.app_secret)
+            jsapi_ticket, ttl = refresh_wxmp_jsapi_ticket(jsapi_config.wxop_app_code, access_token, jsapi_ticket_to_refresh or jsapi_ticket)
+        else:
+            access_token = get_wxmp_access_token(wxop_app_code, app_id, app_secret)
+            jsapi_ticket, ttl = refresh_wxmp_jsapi_ticket(wxop_app_code, access_token, jsapi_ticket_to_refresh or jsapi_ticket)
     return DictObject(jsapi_ticket=jsapi_ticket, expires_in=ttl) if with_ttl else jsapi_ticket
 
 
 def refresh_wxmp_jsapi_ticket(wxop_app_code, access_token, jsapi_ticket_to_refresh):
-    with redis().lock('lock:refresh-wxmp-access-token:{}'.format(wxop_app_code), timeout=2 * 60):
+    with redis().lock('lock:refresh-wxmp-jsapi-ticket:{}'.format(wxop_app_code), timeout=2 * 60):
         with redis().pipeline() as pipe:
             pipe.get(WXMP_JSAPI_TICKET_KEY_TPL.format(wxop_app_code))
             pipe.ttl(WXMP_JSAPI_TICKET_KEY_TPL.format(wxop_app_code))
@@ -50,13 +54,13 @@ def request_wxmp_jsapi_ticket(access_token):
                                 max_retries=Retry(total=5, backoff_factor=0.2))
         response.raise_for_status()
     except Exception:
-        LOGGER.exception('wxmp request access token exception-thrown')
+        LOGGER.exception('wxmp request jsapi ticket exception-thrown')
         raise
     else:
         result = objectify(response.json())
         if hasattr(result, 'ticket'):
-            LOGGER.info('wxmp request access token succeeded: %(result)s, %(access_token)s', {'result': result, 'access_token': access_token})
+            LOGGER.info('wxmp request jsapi ticket succeeded: %(result)s, %(access_token)s', {'result': result, 'access_token': access_token})
             return result.ticket, result.expires_in
         else:
-            LOGGER.error('wxmp request access token failed: %(result)s', {'result': result})
-            raise Exception('wxmp request access token failed: {}'.format(result))
+            LOGGER.error('wxmp request jsapi ticket failed: %(result)s', {'result': result})
+            raise Exception('wxmp request jsapi ticket failed: {}'.format(result))
--- src/veil/backend/weixin/js_sdk.py
+++ src/veil/backend/weixin/js_sdk.py
@@ -12,10 +12,12 @@ def get_js_sdk_sign(params):
     return hashlib.sha1('&'.join('{}={}'.format(k, params[k]) for k in sorted(params))).hexdigest()
 
 
-def get_js_sdk_config(wxop_app_code, app_id, app_secret, url, debug, js_api_list):
+def get_js_sdk_config(wxop_app_code, app_id, app_secret, url, debug, js_api_list, jsapi_config=None):
     debug = to_bool(debug)
     timestamp = get_current_timestamp()
     noncestr = uuid4().get_hex()
-    params = DictObject(jsapi_ticket=get_wxmp_jsapi_ticket(wxop_app_code, app_id, app_secret), noncestr=noncestr, timestamp=timestamp, url=url)
+    params = DictObject(jsapi_ticket=get_wxmp_jsapi_ticket(wxop_app_code, app_id, app_secret, jsapi_config=jsapi_config), noncestr=noncestr,
+                        timestamp=timestamp, url=url)
     signature = get_js_sdk_sign(params)
-    return DictObject(debug=debug, appId=app_id, timestamp=timestamp, nonceStr=noncestr, signature=signature, jsApiList=js_api_list)
+    return DictObject(debug=debug, appId=jsapi_config.app_id if jsapi_config else app_id, timestamp=timestamp, nonceStr=noncestr, signature=signature,
+                      jsApiList=js_api_list)
