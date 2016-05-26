51715ce4bc2992dc59f97b2a18405f04444d6c69
TianZhen <tianzhen@honovation.com>
2016-05-26 11:50:36 +0800

    aisino shared library installer

--- src/veil/backend/aisino/aisino_installer.py
+++ src/veil/backend/aisino/aisino_installer.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function, division
+import os
 
 from veil.environment import *
 from veil.server.config import *
 from veil.server.os import *
 from veil.utility.setting import *
+from veil.utility.shell import *
 from veil_installer import *
 
 from .aisino import REQUEST_AND_RESPONSE_LOG_DIRECTORY_BASE
+
+AISINO_SHARED_LIBRARY_CONF_PATH = '/etc/ld.so.conf.d/aisino.conf'
+AISINO_SHARED_LIBRARY_PATH = DEPENDENCY_DIR / 'aisino'
+RESOURCE_KEY = 'veil.backend.aisino.aisino_invoice_resource'
+RESOURCE_VERSION = '1'
+
 
 _config = None
 
@@ -17,6 +25,7 @@ add_application_sub_resource('aisino_invoice', lambda config: aisino_invoice_res
 @composite_installer
 def aisino_invoice_resource(payer_id, payer_name, payer_auth_code, payer_address, payer_telephone, payer_bank_name, payer_bank_account_no, ebp_code,
                             registration_no, operator_name):
+    install_aisino_shared_library()
     resources = list(BASIC_LAYOUT_RESOURCES)
     resources.append(directory_resource(path=REQUEST_AND_RESPONSE_LOG_DIRECTORY_BASE, owner=CURRENT_USER, group=CURRENT_USER_GROUP))
     resources.append(file_resource(path=VEIL_ETC_DIR / 'aision_invoice.cfg', content=render_config('aision_invoice.cfg.j2',
@@ -43,3 +52,28 @@ def aisino_invoice_config():
     if _config is None:
         _config = load_aisino_invoice_config()
     return _config
+
+
+def install_aisino_shared_library():
+    url = '{}/libSOFJni_x64.so.1'.format(DEPENDENCY_URL)
+    dest_path = DEPENDENCY_DIR / 'aisino'
+    if not dest_path.exists():
+        dest_path.mkdir()
+    destination = dest_path / 'libSOFJni_x64.so.1'
+    if not os.path.exists(destination):
+        shell_execute('wget -c {} -O {}'.format(url, destination))
+    if is_shared_library_installed():
+        if VEIL_ENV_TYPE in {'development', 'test'} and RESOURCE_VERSION != get_resource_latest_version(RESOURCE_KEY):
+            set_resource_latest_version(RESOURCE_KEY, RESOURCE_VERSION)
+        return
+    install_resource(file_resource(path=AISINO_SHARED_LIBRARY_CONF_PATH, content=AISINO_SHARED_LIBRARY_PATH))
+    shell_execute('ldconfig')
+    if VEIL_ENV_TYPE in {'development', 'test'}:
+        set_resource_latest_version(RESOURCE_KEY, RESOURCE_VERSION)
+
+
+def is_shared_library_installed():
+    if not os.path.exists(AISINO_SHARED_LIBRARY_CONF_PATH):
+        return False
+    with open(AISINO_SHARED_LIBRARY_CONF_PATH, 'rb') as f:
+        return AISINO_SHARED_LIBRARY_PATH == f.read()
