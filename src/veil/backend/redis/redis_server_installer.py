75232db904cb7a254fb66950d3312e80fa26340f
Eric Du <eric@honovation.com>
2016-06-06 18:58:31 +0800

    @monitor recursively create directory for log-buffer redis

--- src/veil/backend/redis/redis_server_installer.py
+++ src/veil/backend/redis/redis_server_installer.py
@@ -3,13 +3,13 @@ from veil.profile.installer import *
 
 
 @composite_installer
-def redis_server_resource(purpose, host, port, persisted_by_aof=False, snapshot_configs=None):
+def redis_server_resource(purpose, host, port, persisted_by_aof=False, snapshot_configs=None, recursive=False):
     data_directory = VEIL_DATA_DIR / '{}-redis'.format(purpose.replace('_', '-'))
     return [
         os_ppa_repository_resource(name='rwky/redis'), # for latest redis-server
         os_package_resource(name='redis-server'),
         os_service_auto_starting_resource(name='redis-server', state='not_installed'),
-        directory_resource(path=data_directory, owner=CURRENT_USER, group=CURRENT_USER_GROUP, mode=0770),
+        directory_resource(path=data_directory, owner=CURRENT_USER, group=CURRENT_USER_GROUP, mode=0770, recursive=recursive),
         file_resource(path=VEIL_ETC_DIR / '{}-redis.conf'.format(purpose.replace('_', '-')),
                       content=render_config('redis-server.conf.j2',
                                             config={'host': host, 'port': port, 'data_directory': data_directory, 'persisted_by_aof': persisted_by_aof,
--- src/veil/environment/monitor_setting.py
+++ src/veil/environment/monitor_setting.py
@@ -6,8 +6,9 @@ from veil.backend.redis_setting import redis_program
 
 def monitor_programs(config):
     programs = merge_multiple_settings(
-        redis_program('log_buffer', config.log_buffer_redis_host, config.log_buffer_redis_port, snapshot_configs=[
-            DictObject(interval=interval, changed_keys=changed_keys) for interval, changed_keys in [(900, 1), (300, 10), (60, 10000)]]),
+        redis_program('log_buffer', config.log_buffer_redis_host, config.log_buffer_redis_port,
+                      snapshot_configs=[DictObject(interval=interval, changed_keys=changed_keys) for interval, changed_keys in
+                                        [(900, 1), (300, 10), (60, 10000)]], recursive=True),
         {'elasticsearch': {
             'environment_variables': {'ES_HEAP_SIZE': config.es_heap_size},
             'run_in_directory': '/usr/share/elasticsearch',
