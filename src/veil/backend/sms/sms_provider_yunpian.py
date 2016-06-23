dd59e5ef04d9225861fa9a335a0e26c1fab018e3
TianZhen <tianzhen@honovation.com>
2016-06-23 13:11:21 +0800

    fix: yunpian single send interface

--- src/veil/backend/sms/sms_provider_yunpian.py
+++ src/veil/backend/sms/sms_provider_yunpian.py
@@ -12,6 +12,7 @@ LOGGER = logging.getLogger(__name__)
 
 SMS_PROVIDER_ID = 2
 SEND_SMS_URL = 'https://sms.yunpian.com/v2/sms/batch_send.json'
+SINGLE_SEND_SMS_URL = 'https://sms.yunpian.com/v2/sms/single_send.json'
 SEND_VOICE_URL = 'https://voice.yunpian.com/v2/voice/send.json'
 QUERY_BALANCE_URL = 'https://sms.yunpian.com/v2/user/get.json'
 MAX_SMS_RECEIVERS = 1000
@@ -74,7 +75,52 @@ class YunpianSMService(SMService):
             receivers = [receivers]
         return [r for r in chunks(receivers, MAX_SMS_RECEIVERS)]
 
+    def single_send(self, receiver, message, sms_code, transactional, promotional=False):
+        if not self.config:
+            self.config = yunpian_sms_client_config()
+        api_key = self.config.apikey if not promotional else self.config.promotion_apikey
+        message = message.encode('UTF-8')
+        data = {'apikey': api_key, 'mobile': receiver, 'text': message}
+        response = None
+        try:
+            # retry at most 2 times upon connection timeout or 500 errors, back-off 2 seconds (avoid IP blocking due to too frequent queries)
+            response = requests.post(SINGLE_SEND_SMS_URL, data=data, timeout=(3.05, 9),
+                                     headers={'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'},
+                                     max_retries=Retry(total=2, read=False, method_whitelist={'POST'}, status_forcelist={502, 503, 504}, backoff_factor=2))
+            response.raise_for_status()
+        except ReadTimeout:
+            if transactional:
+                LOGGER.exception('yunpian sms send ReadTimeout exception for transactional message: %(sms_code)s, %(receiver)s',
+                                 {'sms_code': sms_code, 'receiver': receiver})
+                raise
+            else:
+                LOGGER.exception('yunpian sms send ReadTimeout exception for marketing message: %(sms_code)s, %(receiver)s',
+                                 {'sms_code': sms_code, 'receiver': receiver})
+        except Exception as e:
+            LOGGER.exception('yunpian sms send exception-thrown: %(sms_code)s, %(receiver)s, %(message)s, %(response)s', {
+                'sms_code': sms_code, 'receiver': receiver, 'message': e.message, 'response': response.text if response else ''
+            })
+            if response and response.status_code == 400:
+                result = objectify(response.json())
+                if result.code in IGNORE_CODES:
+                    return
+            raise
+        else:
+            result = objectify(response.json())
+            if result.code == 0:
+                LOGGER.info('yunpian sms send succeeded: %(sms_code)s, %(receiver)s', {'sms_code': sms_code, 'receiver': receiver})
+            else:
+                LOGGER.error('yunpian sms send failed: %(sms_code)s, %(response)s, %(receiver)s', {
+                    'sms_code': sms_code, 'response': response.text, 'receiver': receiver
+                })
+                retry_mobiles = [receiver] if result.code in RETRY_CODES else []
+                raise SendError('yunpian sms send failed: {}, {}'.format(sms_code, receiver), retry_mobiles)
+
     def send(self, receivers, message, sms_code, transactional, promotional=False):
+        for receiver in receivers:
+            self.single_send(receiver, message, sms_code, transactional, promotional=promotional)
+    
+    def batch_send(self, receivers, message, sms_code, transactional, promotional=False):
         if not self.config:
             self.config = yunpian_sms_client_config()
         api_key = self.config.apikey if not promotional else self.config.promotion_apikey
