e0bbb29c1d4779e09a651714e0157574c77351d8
Guo Chen <chenguo@honovation.com>
2016-08-08 15:19:00 +0800

    refactor

--- static/veil.js
+++ static/veil.js
@@ -304,12 +304,10 @@ veil.resource.update = function (options) {
     };
 
     // TODO: remove this code after 2017.08.01
-    if (navigator.appVersion.indexOf('MQQBrowser') !== -1) {
-        var qqBrowser = navigator.appVersion.match(/\sMQQBrowser\/\d\.*\d/);
-        if (qqBrowser) {
-            if (parseFloat(qqBrowser[0].split('/')[1]) < 5.8) {
-                $.extend(_, {type: 'POST', url: _.url + '?_method=PUT'});
-            }
+    var qqBrowser = navigator.appVersion.match(/\sMQQBrowser\/\d\.*\d/);
+    if (qqBrowser) {
+        if (parseFloat(qqBrowser[0].split('/')[1]) < 5.8) {
+            $.extend(_, {type: 'POST', url: _.url + '?_method=PUT'});
         }
     }
 
@@ -364,12 +362,10 @@ veil.resource.patch = function (options) {
     };
 
     // TODO: remove this code after 2017.08.01
-    if (navigator.appVersion.indexOf('MQQBrowser') !== -1) {
-        var qqBrowser = navigator.appVersion.match(/\sMQQBrowser\/\d\.*\d/);
-        if (qqBrowser) {
-            if (parseFloat(qqBrowser[0].split('/')[1]) < 5.8) {
-                $.extend(_, {type: 'POST', url: _.url + '?_method=PATCH'});
-            }
+    var qqBrowser = navigator.appVersion.match(/\sMQQBrowser\/\d\.*\d/);
+    if (qqBrowser) {
+        if (parseFloat(qqBrowser[0].split('/')[1]) < 5.8) {
+            $.extend(_, {type: 'POST', url: _.url + '?_method=PATCH'});
         }
     }
 
