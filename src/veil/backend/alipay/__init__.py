ce891ee6e1244d7982312d5dfc3472fc7db4101a
TianZhen <tianzhen@honovation.com>
2016-11-09 15:56:06 +0800

    feat: alipay without-password refund interface and refund notification handler

--- src/veil/backend/alipay/__init__.py
+++ src/veil/backend/alipay/__init__.py
@@ -11,6 +11,9 @@ with veil_component.init_component(__name__):
     from .alipay_payment import make_alipay_order_str
     from .alipay_payment import verify_alipay_sync_notification
     from .alipay_payment import close_alipay_trade
+    from .alipay_payment import refund
+    from .alipay_payment import process_refund_notification
+    from .alipay_payment import process_dback_notification
 
     from .alipay_client_installer import alipay_client_resource
     from .alipay_client_installer import alipay_client_config
@@ -26,6 +29,9 @@ with veil_component.init_component(__name__):
         make_alipay_order_str.__name__,
         verify_alipay_sync_notification.__name__,
         close_alipay_trade.__name__,
+        refund.__name__,
+        process_refund_notification.__name__,
+        process_dback_notification.__name__,
 
         alipay_client_resource.__name__,
         alipay_client_config.__name__,
--- src/veil/backend/alipay/alipay_payment.py
+++ src/veil/backend/alipay/alipay_payment.py
@@ -13,19 +13,34 @@ import rsa
 from veil.frontend.cli import *
 from veil.profile.model import *
 from veil.profile.web import *
+from veil.utility.xml import parse_xml
 from .alipay_client_installer import alipay_client_config
 
 LOGGER = logging.getLogger(__name__)
 
 EVENT_ALIPAY_TRADE_PAID = define_event('alipay-trade-paid')  # valid notification
+EVENT_ALIPAY_REFUND_NOTIFIED = define_event('alipay-refund-notified')
+EVENT_ALIPAY_DBACK_NOTIFIED = define_event('alipay-dback-notified')
 
 PAYMENT_URL = 'https://mapi.alipay.com/gateway.do'
 VERIFY_URL = PAYMENT_URL
+REFUND_URL = PAYMENT_URL
 
 NOTIFIED_FROM_RETURN_URL = 'return_url'
 NOTIFIED_FROM_NOTIFY_URL = 'notify_url'
 NOTIFIED_FROM_PAYMENT_QUERY = 'payment_query'
 NOTIFICATION_RECEIVED_SUCCESSFULLY_MARK = 'success'  # alipay require this 7 characters to be returned to them
+ALIPAY_REFUND_SEQ_NO_LENGTH_MIN = 3
+ALIPAY_REFUND_SEQ_NO_LENGTH_MAX = 24
+ALIPAY_REFUND_DATA_LIST_LENGTH_MAX = 1000
+ALIPAY_REFUND_RESPONSE_SUCCESS_MARK = 'T'
+ALIPAY_REFUND_RESPONSE_FAIL_MARK = 'F'
+ALIPAY_REFUND_RESPONSE_PROCESSING_MARK = 'P'
+ALIPAY_REFUND_RESPONSE_DUPLICATE_BATCH_NO = 'DUPLICATE_BATCH_NO'
+ALIPAY_REFUND_RESULT_SUCCESS_MARK = 'SUCCESS'
+ALIPAY_DBACK_RESULT_SUCCESS_MARK = 'S'
+ALIPAY_DBACK_RESULT_FAIL_MARK = 'F'
+ALIPAY_DBACK_RESULT_WAITING_MARK = 'I'
 
 
 def make_alipay_order_str(out_trade_no, subject, body, total_fee, notify_url, minutes_to_expire):
@@ -310,3 +325,126 @@ def close_alipay_trade(out_trade_no):
                 'params': params,
                 'error': arguments.error
             })
+
+
+def refund(refund_date, seq_no, refund_data_list, notify_url, dback_notify_url):
+    """
+    alipay batch refund interface without password
+
+    :param refund_date: 退款日期，用来生成batch_no
+    :param seq_no: 批量退款流水号（最大24位），用来生成batch_no
+    :param refund_data_list: 退款数据列表，最多1000笔，每个元素为一个DictObject(trade_no=..., amount=Decimal object, reason=...)，其中reason中不能包含: ^, |, $, #
+    :param notify_url: 退款结果异步通知URL
+    :param dback_notify_url: 充退结果异步通知URL
+    :return: 退款批次号
+    """
+    if not refund_data_list:
+        return
+    if len(str(seq_no)) > ALIPAY_REFUND_SEQ_NO_LENGTH_MAX:
+        raise AlipayRefundException('invalid seq_no length')
+    if len(refund_data_list) > ALIPAY_REFUND_DATA_LIST_LENGTH_MAX:
+        raise AlipayRefundException('invalid refund_data_list length')
+    if any(e in rd.reason for e in {'^', '|', '$', '#'} for rd in refund_data_list):
+        raise AlipayRefundException('invalid refund reason in refund_data_list')
+    if len({rd.trade_no for rd in refund_data_list}) != len(refund_data_list):
+        raise AlipayRefundException('need merge refund data with same trade no')
+    batch_no = '{}{}'.format(refund_date.strftime('%Y%m%d'), str(seq_no).zfill(ALIPAY_REFUND_SEQ_NO_LENGTH_MIN))
+    detail_data = '#'.join('{}^{}^{}'.format(rd.trade_no, rd.amount, rd.reason) for rd in refund_data_list)
+    data = dict(
+        service='refund_fastpay_by_platform_nopwd',
+        partner=alipay_client_config().partner_id,
+        _input_charset='UTF-8',
+        notify_url=notify_url,
+        dback_notify_url=dback_notify_url,
+        batch_no=batch_no,
+        refund_date=get_current_time_in_client_timezone().strftime('%Y-%m-%d %H:%M:%S'),
+        batch_num=len(refund_data_list),
+        detail_data=detail_data)
+    data['sign'] = sign_md5(data)
+    data['sign_type'] = 'MD5'
+
+    response = None
+    try:
+        response = requests.get(REFUND_URL, data=data, timeout=(3.05, 9), max_retries=Retry(total=3, backoff_factor=0.2))
+        response.raise_for_status()
+    except Exception:
+        LOGGER.exception('request alipay refund got exception: %(response)s, %(data)s', {'response': response.text if response else '', 'data': data})
+        raise
+    else:
+        LOGGER.debug(response.text)
+        result = parse_xml(response.text)
+        if result.is_success == ALIPAY_REFUND_RESPONSE_SUCCESS_MARK:
+            return batch_no
+        elif result.is_success == ALIPAY_REFUND_RESPONSE_FAIL_MARK:
+            if result.error == ALIPAY_REFUND_RESPONSE_DUPLICATE_BATCH_NO:
+                return batch_no
+            raise AlipayRefundException('request alipay refund got failed result: %(error)s', {'error': result.error})
+        elif result.is_success == ALIPAY_REFUND_RESPONSE_PROCESSING_MARK:
+            raise AlipayRefundException('请稍后重试')
+
+
+def process_refund_notification(arguments):
+    """
+    alipay refund asynchronous notification handler
+
+    :param arguments: all arguments submitted to notify_url
+    :return: success
+
+    publish event
+        DictObject(success=False, reason=...) if sign is incorrect or fake notification from alipay or invalid refund results format
+        DictObject(batch_no: 退款批次号, success_num: 成功退款数量, refund_results=[DictObject(trade_no=..., amount=..., success=True/False, [reason=CODE]), ...]
+    """
+    if not is_sign_correct(arguments):
+        LOGGER.error('got refund notification which sign is incorrect: %(arguments)s', {'arguments': arguments})
+        return DictObject(success=False, reason='sign is incorrect')
+    verify_notification_result = validate_notification_from_alipay(arguments.notify_id)
+    if verify_notification_result:
+        LOGGER.error('got refund notification which is not from alipay: %(arguments)s', {'arguments': arguments})
+        return DictObject(success=False, reason='notification is not from alipay')
+    refund_results = []
+    for refund_result in arguments.result_details.split('#'):
+        try:
+            trade_no, amount, result_code = refund_result.split('^')
+        except Exception:
+            LOGGER.error('got refund notification which refund result format is unexpected: %(result)s', {'result': refund_result})
+            return DictObject(success=False, reason='invalid refund results format')
+        else:
+            if result_code == ALIPAY_REFUND_RESULT_SUCCESS_MARK:
+                refund_results.append(DictObject(trade_no=trade_no, amount=Decimal(amount), success=True))
+            else:
+                refund_results.append(DictObject(trade_no=trade_no, amount=Decimal(amount), success=False, reason=result_code))
+    result = DictObject(batch_no=arguments.batch_no, success_num=arguments.success_num, refund_results=refund_results)
+    publish_event(EVENT_ALIPAY_REFUND_NOTIFIED, result=result)
+    return NOTIFICATION_RECEIVED_SUCCESSFULLY_MARK
+
+
+def process_dback_notification(arguments):
+    """
+    alipay dback notification handler
+
+    :param arguments: all arguments submitted to notify_url
+    :return: success
+
+    publish event
+        DictObject(success=False, reason=...) if sign is incorrect or fake notification from alipay
+        DictObject(trade_no: 原支付交易号, refund_id: 退款id, refund_batch_no: 退款批次号, refund_to_card_no: 退还到的银行卡号或支付宝账户,
+            refund_to_bank_name=退还到的银行或支付宝, success=True/False [, reason=...])
+    """
+    if not is_sign_correct(arguments):
+        LOGGER.error('got dback notification which sign is incorrect: %(arguments)s', {'arguments': arguments})
+        return DictObject(success=False, reason='sign is incorrect')
+    verify_notification_result = validate_notification_from_alipay(arguments.notify_id)
+    if verify_notification_result:
+        LOGGER.error('got dback notification which is not from alipay: %(arguments)s', {'arguments': arguments})
+        return DictObject(success=False, reason='notification is not from alipay')
+    result = DictObject(trade_no=arguments.trade_no, refund_id=arguments.refund_id, refund_batch_no=arguments.refund_batch_no,
+                        refund_to_card_no=arguments.card_no, refund_to_bank_name=arguments.bank_name,
+                        success=arguments.status == ALIPAY_DBACK_RESULT_SUCCESS_MARK)
+    if arguments.status in (ALIPAY_DBACK_RESULT_WAITING_MARK, ALIPAY_DBACK_RESULT_FAIL_MARK):
+        result.reason = '退还至原支付卡失败，在用户余额中'
+    publish_event(EVENT_ALIPAY_DBACK_NOTIFIED, result=result)
+    return NOTIFICATION_RECEIVED_SUCCESSFULLY_MARK
+
+
+class AlipayRefundException(Exception):
+    pass
