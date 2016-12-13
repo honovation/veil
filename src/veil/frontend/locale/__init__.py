c349ec98470a56f1899076010e7b907d4d7dab11
Eric Du <eric@honovation.com>
2016-12-13 10:45:44 +0800

    fix

--- src/veil/frontend/locale/__init__.py
+++ src/veil/frontend/locale/__init__.py
@@ -8,7 +8,6 @@ with veil_component.init_component(__name__):
     from .i18n import get_current_locale
     from .i18n import get_default_locale
     from .i18n import _
-    from .i18n import install_null_translation
     from .l10n import parse_epoch_datetime
     from .l10n import render_datetime_range
 
@@ -21,7 +20,6 @@ with veil_component.init_component(__name__):
         get_current_locale.__name__,
         get_default_locale.__name__,
         _.__name__,
-        install_null_translation.__name__,
         # from l10n
         parse_epoch_datetime.__name__,
         render_datetime_range.__name__,
