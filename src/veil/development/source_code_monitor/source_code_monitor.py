60830b9b581e81077e06f1c4b1b3060564214105
TianZhen <tianzhen@honovation.com>
2017-01-19 13:18:44 +0800

    feat: source code monitor by watchdog, closed #51

--- src/veil/development/source_code_monitor/source_code_monitor.py
+++ src/veil/development/source_code_monitor/source_code_monitor.py
@@ -3,8 +3,9 @@ import logging
 import functools
 import time
 import threading
-import discipline_coach
 import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 from veil_component import VEIL_ENV, VEIL_HOME, VEIL_FRAMEWORK_HOME
 from veil.frontend.website_setting import list_website_components
 
@@ -35,88 +36,22 @@ class SourceCodeMonitor(threading.Thread):
     def __init__(self):
         super(SourceCodeMonitor, self).__init__()
         self.daemon = True
-        self.init_app_base_version = None
-        self.init_app_changes = None
-        self.init_framework_base_version = None
-        self.init_framework_changes = None
-        try:
-            self.init_app_base_version, self.init_app_changes = discipline_coach.get_git_dir_version(VEIL_HOME, depress_log=True)
-        except Exception:
-            pass
-        try:
-            self.init_framework_base_version, self.init_framework_changes = discipline_coach.get_git_dir_version(VEIL_FRAMEWORK_HOME, depress_log=True)
-        except Exception:
-            pass
 
     def run(self):
-        while True:
-            try:
-                time.sleep(0.5)
-                if self.detect_change():
-                    os._exit(1)
-            except Exception:
-                LOGGER.exception('source code monitor died')
+        application_dir_observer = Observer()
+        application_dir_observer.schedule(ReloadEventHandler(), VEIL_HOME, recursive=True)
+        application_dir_observer.start()
 
-    def detect_change(self):
-        return self.detect_app_change() or self.detect_framework_change()
-
-    def detect_framework_change(self):
-        while 1:
-            try:
-                current_framework_base_version, current_framework_changes = discipline_coach.get_git_dir_version(VEIL_FRAMEWORK_HOME, depress_log=True)
-            except Exception:
-                pass
-            else:
-                if self.init_framework_base_version:
-                    if self.init_framework_base_version != current_framework_base_version:
-                        LOGGER.info('framework base version changed: reload now')
-                        self.init_framework_base_version = current_framework_base_version
-                        return True
-                else:
-                    self.init_framework_base_version = current_framework_base_version
-
-                if self.init_framework_changes:
-                    if is_python_source_code_changed(self.init_framework_changes, current_framework_changes):
-                        LOGGER.info('framework python source code changed: reload now')
-                        self.init_framework_changes = current_framework_changes
-                        return True
-                else:
-                    self.init_framework_changes = current_framework_changes
-                return False
-
-    def detect_app_change(self):
-        while 1:
-            try:
-                current_app_base_version, current_app_changes = discipline_coach.get_git_dir_version(VEIL_HOME, depress_log=True)
-            except Exception:
-                pass
-            else:
-                if self.init_app_base_version:
-                    if self.init_app_base_version != current_app_base_version:
-                        LOGGER.info('application base version changed: reload now')
-                        self.init_app_base_version = current_app_base_version
-                        return True
-                else:
-                    self.init_app_base_version = current_app_base_version
-
-                if self.init_app_changes:
-                    if is_python_source_code_changed(self.init_app_changes, current_app_changes):
-                        LOGGER.info('application python source code changed: reload now')
-                        self.init_app_changes = current_app_changes
-                        return True
-                else:
-                    self.init_app_changes = current_app_changes
-                return False
+        framework_dir_observer = Observer()
+        framework_dir_observer.schedule(ReloadEventHandler(), VEIL_FRAMEWORK_HOME, recursive=True)
+        framework_dir_observer.start()
 
 
-def is_python_source_code_changed(init_changes, current_changes):
-    init_change_set = set(init_changes.items())
-    current_change_set = set(current_changes.items())
-    diff_change_set = init_change_set ^ current_change_set
-    for path, version in diff_change_set:
-        if '.py' in path:
-            return True
-    return False
+class ReloadEventHandler(FileSystemEventHandler):
+    def on_any_event(self, event):
+        if not event.is_directory and ('.py' in event.src_path or 'DEP-DYNAMIC-MANUAL' in event.src_path):
+            LOGGER.info('event type: %(type)s, path: %(path)s', {'type': event.event_type, 'path': event.src_path})
+            os._exit(1)
 
 
 def load_components(components):
