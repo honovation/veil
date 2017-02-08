409d66ad479e5aa77d10b81a36dea2f84ab359e5
TianZhen <tianzhen@honovation.com>
2017-02-08 11:07:43 +0800

    refactor: elk is not started automatically after installation

--- src/veil/environment/monitor/elk.py
+++ src/veil/environment/monitor/elk.py
@@ -11,15 +11,11 @@ def elk_resource(config):
         apt_repository_resource(name='elk', key_url='https://artifacts.elastic.co/GPG-KEY-elasticsearch', definition=repository_definition),
 
         os_package_resource(name='elasticsearch'),
-        os_service_auto_starting_resource(name='elasticsearch', state='not_installed'),
         file_resource(path='/etc/elasticsearch/elasticsearch.yml', keep_origin=True,
                       content=render_config('elasticsearch.yml.j2', elasticsearch_cluster=VEIL_ENV.name, **config)),
 
         os_package_resource(name='logstash'),
-        os_service_auto_starting_resource(name='logstash', state='not_installed'),
-        os_service_auto_starting_resource(name='logstash-web', state='not_installed'),
         file_resource(path='/etc/logstash/conf.d/10-logstash.conf', content=render_config('logstash.conf.j2', **config)),
 
         os_package_resource(name='kibana'),
-        os_service_auto_starting_resource(name='kibana', state='not_installed'),
     ]
