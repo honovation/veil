6cba914c8710aafd60c07d88e106f64e769b3398
TianZhen <tianzhen@honovation.com>
2017-02-17 00:43:10 +0800

    refactor: change redis-server repository

--- src/veil/backend/redis/redis_server_installer.py
+++ src/veil/backend/redis/redis_server_installer.py
@@ -6,7 +6,7 @@ from veil.profile.installer import *
 def redis_server_resource(purpose, host, port, persisted_by_aof=False, snapshot_configs=None, recursive=False):
     data_directory = VEIL_DATA_DIR / '{}-redis'.format(purpose.replace('_', '-'))
     return [
-        os_ppa_repository_resource(name='rwky/redis'), # for latest redis-server
+        os_ppa_repository_resource(name='chris-lea/redis-server'), # for latest redis-server
         os_package_resource(name='redis-server'),
         os_service_auto_starting_resource(name='redis-server', state='not_installed'),
         directory_resource(path=data_directory, owner=CURRENT_USER, group=CURRENT_USER_GROUP, mode=0770, recursive=recursive),
