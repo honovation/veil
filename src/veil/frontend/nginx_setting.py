d981a9a3740865cc56cb8b46532b93be8ca17d8f
Eric Du <eric@honovation.com>
2017-05-23 15:25:58 +0800

    certbot: install, obtain and renew

--- src/veil/frontend/nginx_setting.py
+++ src/veil/frontend/nginx_setting.py
@@ -30,7 +30,7 @@ def nginx_program(servers, enable_compression=False, base_domain_names=(), has_b
 
 def nginx_server(server_name, listen, locations, upstreams=None, error_page=None, error_page_dir=None, ssl=False,
                  use_certbot=False, default_server=False, additional_listens=(), **kwargs):
-    assert not use_certbot or ssl
+    assert not use_certbot or ssl and not any(k in kwargs and kwargs['k'] for k in ('ssl_certificate', 'ssl_certificate_key', 'ssl_trusted_certificate', 'ssl_stapling', 'ssl_stapling_verify'))
     return {
         server_name: dict({
             'listen': int(listen),
--- src/veil/server/supervisor/supervisorctl.py
+++ src/veil/server/supervisor/supervisorctl.py
@@ -1,13 +1,20 @@
 from __future__ import unicode_literals, print_function, division
 import logging
 from veil.environment import *
+from veil.frontend.cli import *
 from veil.utility.shell import *
 
 LOGGER = logging.getLogger(__name__)
 
 
+@script('reload-nginx')
+def reload_nginx(program_name='nginx'):
+    supervisorctl('signal', 'HUP', program_name, capture=True)
+
+
 def supervisorctl(action, *arguments, **kwargs):
-    return shell_execute('supervisorctl -c {} {} {}'.format(VEIL_ETC_DIR / 'supervisor.cfg', action, ' '.join(arguments)), **kwargs)
+    return shell_execute(
+        'supervisorctl -c {} {} {}'.format(VEIL_ETC_DIR / 'supervisor.cfg', action, ' '.join(arguments)), **kwargs)
 
 
 def is_supervisord_running():
