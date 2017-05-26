41c004e0ffe662207f1d1af6717475fd4003ff3e
Eric Du <eric@honovation.com>
2017-05-26 15:59:33 +0800

    remove --quiet

--- src/veil/server/certbot/certbot.py
+++ src/veil/server/certbot/certbot.py
@@ -36,7 +36,7 @@ def install():
 def certonly(domain_name, staging=False):
     staging_option = '--staging' if staging else ''
     email = get_application().CERTBOT_EMAIL
-    cmd = 'certbot certonly {} --quiet --non-interactive --no-eff-email --agree-tos --email {} --keep-until-expiring ' \
+    cmd = 'certbot certonly {} --non-interactive --no-eff-email --agree-tos --email {} --keep-until-expiring ' \
           '--allow-subset-of-names --expand --renew-with-new-domains --rsa-key-size 4096 --must-staple --webroot ' \
           '-w /var/www/html/ -d {} --post-hook "{}"'.format(staging_option, email, domain_name, RELOAD_NGINX_CMD)
     shell_execute(cmd, capture=True)
@@ -44,7 +44,7 @@ def certonly(domain_name, staging=False):
 
 @script('renew')
 def renew():
-    shell_execute('certbot renew --quiet --rsa-key-size 4096 --post-hook "{}"'.format(RELOAD_NGINX_CMD), capture=True)
+    shell_execute('certbot renew --rsa-key-size 4096 --post-hook "{}"'.format(RELOAD_NGINX_CMD), capture=True)
 
 
 @script('renew-termly')
