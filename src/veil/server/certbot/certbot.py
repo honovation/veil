6fc45df14264a6edf55a412f8b45341ab4b6537b
Eric Du <eric@honovation.com>
2017-05-27 18:00:11 +0800

    remove --must-staple

--- src/veil/server/certbot/certbot.py
+++ src/veil/server/certbot/certbot.py
@@ -5,14 +5,14 @@ One single certificate for multiple domains is not supported
 How to use letsencrypt certificate for a website under a domain
 1. update veil app env config to add the parent domain under base_domain_names and deploy
 2. ssh to veil server hosting nginx service
-3. install/upgrade certbot: sudo veil server certbot install
-4. get the certificate for the domain: sudo veil server certbot certonly www.lijiababy.com.cn
+3. install/upgrade certbot: sudo veil :ljmall-public--2/web server certbot install
+4. get the certificate for the domain: sudo veil :ljmall-public--2/web server certbot certonly www.lijiababy.com.cn
 5. update veil app env config to add the website with use_certbot=True
 6. deploy and verify
 
 How to renew letsencrypt certificates
 1. ssh to veil server hosting nginx service
-2. renew letsencrypt certificates and reload nginx if necessary: sudo veil server certbot renew
+2. renew letsencrypt certificates and reload nginx if necessary: sudo veil :ljmall-public--2/web server certbot renew
 
 When switching nginx service to another veil server, need copy /etc/letsencrypt/ to the new veil server
 """
@@ -23,8 +23,6 @@ from veil.utility.shell import *
 from veil.utility.timer import *
 from veil.environment import get_application
 
-RELOAD_NGINX_CMD = 'veil server supervisor reload-nginx'
-
 
 @script('install')
 def install():
@@ -34,17 +32,17 @@ def install():
 
 @script('certonly')
 def certonly(domain_name, staging=False):
-    staging_option = '--staging' if staging else ''
+    staging_option = '--staging' if staging == 'TRUE' else ''
     email = get_application().CERTBOT_EMAIL
     cmd = 'certbot certonly {} --non-interactive --no-eff-email --agree-tos --email {} --keep-until-expiring ' \
-          '--allow-subset-of-names --expand --renew-with-new-domains --rsa-key-size 4096 --must-staple --webroot ' \
-          '-w /var/www/html/ -d {} --post-hook "{}"'.format(staging_option, email, domain_name, RELOAD_NGINX_CMD)
+          '--allow-subset-of-names --expand --renew-with-new-domains --rsa-key-size 4096 --webroot -w /var/www/html/ ' \
+          '-d {}'.format(staging_option, email, domain_name)
     shell_execute(cmd, capture=True)
 
 
 @script('renew')
 def renew():
-    shell_execute('certbot renew --rsa-key-size 4096 --post-hook "{}"'.format(RELOAD_NGINX_CMD), capture=True)
+    shell_execute('certbot renew --rsa-key-size 4096 --post-hook "veil server supervisor reload-nginx"', capture=True)
 
 
 @script('renew-termly')
