302aaaacb04254ccd08fefb48665ec3c2fddb9d2
ChenGuo <chenguo@honovation.com>
2017-06-26 13:38:24 +0800

    feat: custom retry function of tasktiger

--- src/veil/backend/job_queue/__init__.py
+++ src/veil/backend/job_queue/__init__.py
@@ -7,6 +7,7 @@ from .queue_client_installer import queue_client_resource
 with veil_component.init_component(__name__):
 
     from .job_queue import task, periodic, cron_expr, fixed, linear, exponential
+    from .retry import dynamic
 
     def init():
         add_application_sub_resource('queue_client', lambda config: queue_client_resource(**config))
@@ -18,4 +19,6 @@ with veil_component.init_component(__name__):
         fixed.__name__,
         linear.__name__,
         exponential.__name__,
+
+        dynamic.__name__,
     ]
---
+++ src/veil/backend/job_queue/retry.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, division
+
+from tasktiger.exceptions import StopRetry
+
+
+def _dynamic(retry, delay_list):
+    if retry > len(delay_list):
+        raise StopRetry()
+    return delay_list[retry - 1]
+
+
+def dynamic(delay_list):
+    if delay_list:
+        return (_dynamic, (delay_list, ))
+    else:
+        return None
