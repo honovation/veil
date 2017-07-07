a45360d3ee630c6e18999a72a4d057e50756f5cd
TianZhen <tianzhen@honovation.com>
2017-07-07 10:01:33 +0800

    refactor: remove sendgrid job

--- src/veil/backend/sendgrid/__init__.py
+++ src/veil/backend/sendgrid/__init__.py
@@ -3,7 +3,6 @@ from veil_installer import *
 
 with veil_component.init_component(__name__):
     from .sendgrid import send_email
-    from .sendgrid import send_transactional_email_job
 
     from .sendgrid_client_installer import sendgrid_client_resource
 
@@ -14,7 +13,6 @@ with veil_component.init_component(__name__):
     __all__ = [
         # from sendgrid
         send_email.__name__,
-        send_transactional_email_job.__name__,
         # from sendgrid_client_installer
         sendgrid_client_resource.__name__,
     ]
--- src/veil/backend/sendgrid/sendgrid.py
+++ src/veil/backend/sendgrid/sendgrid.py
@@ -90,8 +90,3 @@ def send_email(sender, recipient, subject, text='', html='', category='', email_
             'email_code': email_code,
             'to_addr': to_addr
         })
-
-
-@job('send_transactional_email', retry_every=3 * 60, retry_timeout=3 * 60 * 60)
-def send_transactional_email_job(sender, recipient, subject, text='', html='', category='', email_code=''):
-    send_email(sender, recipient, subject, text, html, category, email_code)
