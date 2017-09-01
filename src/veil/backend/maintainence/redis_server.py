8c24ee95967d210712b99cf1b83aa407e8ac465a
Eric Du <eric@honovation.com>
2017-09-01 09:18:30 +0800

    log redis aof

--- src/veil/backend/maintainence/redis_server.py
+++ src/veil/backend/maintainence/redis_server.py
@@ -1,19 +1,26 @@
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals, print_function, division
+
+import logging
+
 from redis.client import StrictRedis
 from veil.environment import get_current_veil_env, get_current_veil_server
 from veil.model.event import *
 from veil.backend.job_queue import *
 from veil.server.supervisor import *
 
+LOGGER = logging.getLogger(__name__)
+
 
 @task(queue='rewrite_redis_aof', schedule=cron_expr('47 * * * *'))
 def rewrite_redis_aof():
+    LOGGER.info('try to rewrite redis aof periodically')
     _rewrite_redis_aof()
 
 
 @event(EVENT_SUPERVISOR_TO_BE_DOWN)
 def rewrite_redis_aof():
+    LOGGER.info('try to rewrite redis aof before supervisor shutdown')
     _rewrite_redis_aof(get_current_veil_server())
 
 
@@ -27,4 +34,5 @@ def _rewrite_redis_aof(veil_server=None):
         client = StrictRedis(host=host, port=port)
         if client.config_get('appendonly')['appendonly'] != 'yes':
             continue
+        LOGGER.info('rewrite redis aof <%(host)s:%(port)s>', {'host': host, 'port': port})
         client.bgrewriteaof()
