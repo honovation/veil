f56e100799b5e22466b68d84ada94d21eee1744c
TianZhen <tianzhen@honovation.com>
2017-09-01 13:27:06 +0800

    refactor: remove useless

--- src/veil/backend/job_queue/tasktiger_admin_installer.py
+++ src/veil/backend/job_queue/tasktiger_admin_installer.py
@@ -7,6 +7,5 @@ from veil.profile.installer import *
 def tasktiger_admin_resource(host, port, broker_host, broker_port, broker_db=0):
     config = {'host': host, 'port': port, 'broker_host': broker_host, 'broker_port': broker_port, 'broker_db': broker_db}
     return [
-        python_package_resource(name='tasktiger-admin'),
         file_resource(path=VEIL_ETC_DIR / 'tasktiger-admin.cfg', content=render_config('tasktiger-admin.cfg.j2', **config))
     ]
