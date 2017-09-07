9a4e22a41ae0ff5759f91a66acc37cdccbbc0356
Eric Du <eric@honovation.com>
2017-09-07 10:29:18 +0800

    fix: sleep to ensure aof rewrite completed before redis shutdown

--- src/veil/backend/maintainence/redis_server.py
+++ src/veil/backend/maintainence/redis_server.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals, print_function, division
 
 import logging
 
+import time
 from redis import StrictRedis
 from veil.environment import get_current_veil_env, get_current_veil_server
 from veil.model.event import *
@@ -21,13 +22,16 @@ def rewrite_redis_aof():
 @event(EVENT_SUPERVISOR_TO_BE_DOWN)
 def on_supervisor_to_be_down():
     LOGGER.info('try to rewrite redis aof before supervisor shutdown')
-    _rewrite_redis_aof(get_current_veil_server())
+    requested = _rewrite_redis_aof(get_current_veil_server())
+    if requested:
+        time.sleep(10)  # sleep enough time for redis to complete aof rewrite
 
 
 def _rewrite_redis_aof(veil_server=None):
     current_veil_env = get_current_veil_env()
     if not hasattr(current_veil_env.config, 'redis_servers'):
-        return
+        return False
+    requested = False
     for host, port in current_veil_env.config.redis_servers:
         if veil_server and veil_server.internal_ip != host:
             continue
@@ -37,7 +41,10 @@ def _rewrite_redis_aof(veil_server=None):
                 continue
             LOGGER.info('request for redis aof rewrite <%(host)s:%(port)s>', {'host': host, 'port': port})
             client.bgrewriteaof()
+            requested = True
         except Exception as e:
             if 'append only file rewriting already in progress' not in e.message:
+                requested = True
                 LOGGER.exception('Exception thrown while requesting for redis aof rewrite <%(host)s:%(port)s>',
                                  {'host': host, 'port': port})
+    return requested
