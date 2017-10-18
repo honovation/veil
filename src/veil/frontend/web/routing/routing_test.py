9ea6446cbda58e79aad460f3a4ef66404ed73e77
TianZhen <tianzhen@honovation.com>
2017-10-18 14:21:59 +0800

    fix: user agent with unicode string and tests for parse_user_agent

--- src/veil/frontend/web/routing/routing_test.py
+++ src/veil/frontend/web/routing/routing_test.py
@@ -1,6 +1,9 @@
+# -*- coding: UTF-8 -*-
 from __future__ import unicode_literals, print_function, division
 from veil.development.test import TestCase
 from .routing import PathTemplate
+from .routing import parse_user_agent
+
 
 class PathTemplateTest(TestCase):
     def test_simple_template(self):
@@ -17,3 +20,13 @@ class PathTemplateTest(TestCase):
 
     def test_too_many_params(self):
         self.assertRaises(Exception, lambda: PathTemplate('/some', {'id': '.*'}))
+
+
+class ParseUserAgentTest(TestCase):
+    def test_user_agent_with_unicode(self):
+        ua = parse_user_agent(b'Rajax/1 Lenovo_A820t/A820t Android/4.1.2 Display/Lenovo_A820t_S115_AnZhiæ <96>å®¿ Eleme/4.4.1 ID/ea14ab53-4c54-3645-bac9-a81b63d6e7f1; KERNEL_VERSION:3.4.5 API_Level:16 Mozilla/5.0 (Linux; U; Android 4.1.2; zh-cn; Lenovo A820t Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30')
+        self.assertEqual(b'', ua.ua_string)
+
+    def test_user_agent_with_wx(self):
+        ua = parse_user_agent(b'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0_2 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Mobile/15A421 MicroMessenger/6.5.18 NetType/WIFI Language/zh_CN')
+        self.assertTrue(ua.is_from_weixin, 'it should be from weixin')
