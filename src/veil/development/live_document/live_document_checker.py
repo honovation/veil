74a698462da2834a374e6522b1bbee1b75f302c9
TianZhen <tianzhen@honovation.com>
2018-03-14 17:54:13 +0800

    refactor: exec doctest with globals

--- src/veil/development/live_document/live_document_checker.py
+++ src/veil/development/live_document/live_document_checker.py
@@ -5,11 +5,9 @@ from veil.environment import VEIL_HOME
 from veil.utility.encoding import *
 from veil.development.test import *
 
-# imports required in mock functions
-from veil.model.collection import DictObject
-from veil.utility.clock import get_current_time, get_current_date_in_client_timezone
 
 LOGGER = logging.getLogger(__name__)
+
 
 def check_live_document():
     live_doc_path = (VEIL_HOME / '文档')
@@ -18,5 +16,5 @@ def check_live_document():
         return
     for doc in live_doc_path.walkfiles('*.py'):
         LOGGER.info('checking live document: %(doc)s', {'doc': to_unicode(doc)})
-        exec(compile(doc.text(), doc, 'exec'))
+        exec(compile(doc.text(), doc, 'exec'), globals())
         tear_down_fake_test()
