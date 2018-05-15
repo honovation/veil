2d04a0a13c96e63367ec7f81b26868ba539259b1
TianZhen <loooseleaves@gmail.com>
2018-05-15 14:06:42 +0800

    feat: lxd create container

--- src/veil/environment/lxc/__init__.py
+++ src/veil/environment/lxc/__init__.py
@@ -3,18 +3,9 @@ with veil_component.init_component(__name__):
     from .lxc_container_installer import lxc_container_resource
     from .lxc_container_installer import lxc_container_created_resource
     from .lxc_container_installer import lxc_container_in_service_resource
-    from .lxc_container_timezone_installer import lxc_container_timezone_resource
-    from .lxc_container_network_installer import lxc_container_network_resource
-    from .lxc_container_network_installer import lxc_container_name_servers_resource
 
     __all__ = [
-        # from lxc_container_installer
         lxc_container_resource.__name__,
         lxc_container_created_resource.__name__,
         lxc_container_in_service_resource.__name__,
-        # from lxc_container_timezone_installer
-        lxc_container_timezone_resource.__name__,
-        # from lxc_container_network_installer
-        lxc_container_network_resource.__name__,
-        lxc_container_name_servers_resource.__name__
     ]
--- src/veil/environment/lxc/lxc_container_installer.py
+++ src/veil/environment/lxc/lxc_container_installer.py
@@ -1,43 +1,32 @@
 from __future__ import unicode_literals, print_function, division
 import pylxd
-from veil.frontend.cli import script
+import pwd
 from veil.profile.installer import *
 
 LOGGER = logging.getLogger(__name__)
 
 
-def get_lxc_client(verify_ssl=False):
-    config = load_config_from(SECURITY_CONFIG_FILE, 'lxd_cert_path', 'lxd_endpoint', 'lxd_trust_password')
-    cert_path = os.path.expanduser(config.lxd_cert_path)
-    client = pylxd.Client(endpoint=config.lxd_endpoint, cert=('{}/lxd.crt'.format(cert_path), '{}/lxd.key'.format(cert_path)), verify=verify_ssl,
-                          timeout=(3.05, 27))
-    if not client.trusted:
-        client.authenticate(config.lxd_trust_password)
-    return client
+def get_idmap():
+    current_user_pwd_entry = pwd.getpwnam(CURRENT_USER)
+    return current_user_pwd_entry.pw_uid, current_user_pwd_entry.pw_gid
 
 
 @composite_installer
-def lxc_container_resource(container_name, user_name, mac_address, lan_interface, start_order, etc_dir, log_dir,
-                           editorial_dir=None, buckets_dir=None, data_dir=None, memory_limit=None, cpu_share=None,
-                           cpus=None):
-    container_path = as_path('/var/lib/lxc/') / container_name
-    container_rootfs_path = container_path / 'rootfs'
+def lxc_container_resource(container_name, hostname, timezone, user_name, ip_address, gateway, name_servers, start_order, etc_dir, log_dir,
+                           editorial_dir=None, buckets_dir=None, data_dir=None, memory_limit=None, cpu_share=None, cpus=None):
     return [
-        lxc_container_created_resource(container_name=container_name, user_name=user_name),
-        file_resource(path=container_path / 'config',
-                      content=render_config('lxc-container.cfg.j2', name=container_name,
-                                            mac_address=mac_address, lan_interface=lan_interface,
-                                            start_order=start_order, memory_limit=memory_limit, cpu_share=cpu_share,
-                                            cpus=cpus, share_dir=SHARE_DIR, code_dir=VEIL_HOME.parent, etc_dir=etc_dir,
-                                            editorial_dir=editorial_dir, buckets_dir=buckets_dir, data_dir=data_dir,
-                                            log_dir=log_dir, user_name=user_name), keep_origin=True),
-        file_resource(path=container_rootfs_path / 'etc' / 'sysctl.d' / '60-disable-ipv6.conf', content=render_config('disable-ipv6.conf'))
+        lxc_container_created_resource(container_name=container_name, hostname=hostname, timezone=timezone, ip_address=ip_address, gateway=gateway,
+                                       name_servers=name_servers, user_name=user_name, start_order=start_order, memory_limit=memory_limit, cpus=cpus,
+                                       cpu_share=cpu_share, share_dir=SHARE_DIR, code_dir=VEIL_HOME.parent, etc_dir=etc_dir, log_dir=log_dir,
+                                       editorial_dir=editorial_dir, buckets_dir=buckets_dir, data_dir=data_dir)
     ]
 
 
 @atomic_installer
-def lxc_container_created_resource(container_name, user_name, start_order, memory_limit=None, cpus=None, cpu_share=None):
-    client = get_lxc_client()
+def lxc_container_created_resource(container_name, hostname, timezone, user_name, ip_address, gateway, name_servers, start_order, memory_limit=None, cpus=None,
+                                   cpu_share=None, idmap=None, share_dir=None, code_dir=None, etc_dir=None, log_dir=None, editorial_dir=None, buckets_dir=None,
+                                   data_dir=None):
+    client = pylxd.Client()
     installed = client.containers.exists(container_name)
     dry_run_result = get_dry_run_result()
     if dry_run_result is not None:
@@ -48,23 +37,35 @@ def lxc_container_created_resource(container_name, user_name, start_order, memor
     LOGGER.info('create lxc container: %(container_name)s...', {'container_name': container_name})
     user_data = '''
     #cloud-config
-    hostname: {container_name}
+    hostname: {hostname}
+    timezone: {timezone}
     users:
     - name: {user_name}
       gecos: {user_name}
       shell: /bin/bash
       sudo: ALL=(ALL) NOPASSWD:ALL
-    '''.format(container_name=container_name, user_name=user_name)
+    write_files:
+      - path: /etc/sysctl.d/10-disable-ipv6.conf
+        permissions: 0644
+        owner: root
+        content: |
+          net.ipv6.conf.all.disable_ipv6 = 1
+          net.ipv6.conf.default.disable_ipv6 = 1
+          net.ipv6.conf.lo.disable_ipv6 = 1
+    runcmd:
+      - [ systemctl, restart, systemd-sysctl.service ]
+    '''.format(container_name=container_name, hostname=hostname, timezone=timezone, user_name=user_name)
     network_config = '''
     version: 2
     ethernets:
       eth0:
-        addresses: [ 10.25.1.10/16 ]
-        gateway4: 10.25.1.1
+        addresses: [ {ip_address}/16 ]
+        gateway4: {gateway}
         nameservers: 
-          addresses: [ 223.5.5.5, 223.6.6.6 ]
-    '''
-    container_config = {
+          addresses: [ {name_servers} ]
+    '''.format(ip_address=ip_address, gateway=gateway, name_servers=name_servers)
+    idmap = idmap or get_idmap()
+    container_config = DictObject({
         'name': container_name,
         'architecture': 'x86_64',
         'profiles': ['default'],
@@ -73,35 +74,73 @@ def lxc_container_created_resource(container_name, user_name, start_order, memor
             'boot.autostart': 'true',
             'boot.autostart.delay': '3',
             'boot.autostart.priority': start_order,
-            'limits.cpu': cpus,
-            'limits.cpu.allowance': cpu_share,
-            'limits.memory': memory_limit,
             'user.user-data': user_data,
-            'user.network-config': network_config
+            'user.network-config': network_config,
+            'raw.idmap': 'uid {host_uid} 1000\ngid {host_gid} 1000'.format(host_uid=idmap[0], host_gid=idmap[1])
         },
         'devices': {
             'home': {
                 'type': 'disk',
                 'path': '/home/{}'.format(user_name),
                 'source': '/home/{}'.format(user_name)
+            },
+            'share': {
+                'type': 'disk',
+                'path': share_dir,
+                'source': share_dir
+            },
+            'code': {
+                'type': 'disk',
+                'path': code_dir,
+                'source': code_dir
+            },
+            'etc': {
+                'type': 'disk',
+                'path': etc_dir,
+                'source': etc_dir
+            },
+            'log': {
+                'type': 'disk',
+                'path': log_dir,
+                'source': log_dir
             }
         },
         'source': {
             'type': 'image',
             'alias': 'u1804'
         },
-    }
+    })
+    if cpus:
+        container_config.config['limits.cpu'] = cpus
+    if cpu_share:
+        container_config.config['limits.cpu.allowance'] = cpu_share
+    if memory_limit:
+        container_config.config['limits.memory'] = memory_limit
+
+    if editorial_dir:
+        container_config.devices['editorial'] = {
+            'type': 'disk',
+            'path': editorial_dir,
+            'source': editorial_dir
+        }
+    if buckets_dir:
+        container_config.devices['buckets'] = {
+            'type': 'disk',
+            'path': buckets_dir,
+            'source': buckets_dir
+        }
+    if data_dir:
+        container_config.devices['data'] = {
+            'type': 'disk',
+            'path': data_dir,
+            'source': data_dir
+        }
     client.containers.create(container_config, wait=True)
-
-
-@script('t')
-def t():
-    install_resource(lxc_container_created_resource(container_name='con-1', user_name='dejavu', start_order='1024'))
 
 
 @atomic_installer
 def lxc_container_in_service_resource(container_name, restart_if_running=False):
-    client = get_lxc_client()
+    client = pylxd.Client()
     running = client.containers.get(container_name).status_code == 103
     if running:
         action = 'RESTART' if restart_if_running else None
