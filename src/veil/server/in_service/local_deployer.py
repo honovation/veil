9dd9d601112a3676b83750ce4e878bd1bd524d65
TianZhen <loooseleaves@gmail.com>
2018-05-16 15:24:00 +0800

    fix: local deploy

--- src/veil/server/in_service/local_deployer.py
+++ src/veil/server/in_service/local_deployer.py
@@ -19,15 +19,15 @@ def deploy(start_after_deploy='TRUE'):
         check_no_commits_not_pushed()
     shell_execute('veil install veil_installer.component_resource?veil.server.supervisor')
     if VEIL_ENV.is_dev or VEIL_ENV.is_test:
-        shell_execute('veil down')
+        shell_execute('sudo veil down')
     else:
-        shell_execute('systemctl stop veil-server.service')
+        shell_execute('sudo systemctl stop veil-server.service')
     shell_execute('veil install-server')
     if start_after_deploy == 'TRUE':
         if VEIL_ENV.is_dev or VEIL_ENV.is_test:
-            shell_execute('veil up --daemonize')
+            shell_execute('sudo veil up --daemonize')
         else:
-            shell_execute('systemctl start veil-server.service')
+            shell_execute('sudo systemctl start veil-server.service')
         shell_execute('veil migrate')
     if VEIL_ENV.is_dev or VEIL_ENV.is_test:
         check_no_changes_not_committed()
