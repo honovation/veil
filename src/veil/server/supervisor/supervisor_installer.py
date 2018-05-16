1de69413e2c85e35a89d911a38aaed11df3e88ea
TianZhen <loooseleaves@gmail.com>
2018-05-16 15:33:46 +0800

    refactor: supervisord config permission

--- src/veil/server/supervisor/supervisor_installer.py
+++ src/veil/server/supervisor/supervisor_installer.py
@@ -17,12 +17,11 @@ def supervisor_resource(programs, inet_http_server_host=None, inet_http_server_p
             'directory': VEIL_LOG_DIR
         }
     }
+    config = dict(programs=programs, program_groups=program_groups or {}, pid_file='/tmp/supervisor.pid')
+    config = merge_multiple_settings(config, inet_http_server_config, logging_config)
     return [
         python_package_resource(name='supervisor'),
-        file_resource(path=VEIL_ETC_DIR / 'supervisor.cfg',
-                      content=render_config('supervisord.cfg.j2',
-                                            config=merge_multiple_settings(inet_http_server_config, logging_config,
-                                                                           dict(programs=programs, program_groups=program_groups or {},
-                                                                                pid_file='/tmp/supervisor.pid')))),
+        file_resource(path=VEIL_ETC_DIR / 'supervisor.cfg', content=render_config('supervisord.cfg.j2', config=config), owner=CURRENT_USER,
+                      group=CURRENT_USER_GROUP),
         directory_resource(path=VEIL_LOG_DIR, owner=CURRENT_USER, group=CURRENT_USER_GROUP)
     ]
