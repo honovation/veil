1ad477d259b32ca7c71787d71f2a94330d221444
TianZhen <loooseleaves@gmail.com>
2018-05-17 11:13:06 +0800

    feat: redis source code resource

--- src/veil/backend/redis/redis_server_installer.py
+++ src/veil/backend/redis/redis_server_installer.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals, print_function, division
 from veil.profile.installer import *
 
+REDIS_SERVER_SOURCE_CODE_VERSION = '4.0.9'
+
 
 @composite_installer
 def redis_server_resource(purpose, host, port, persisted_by_aof=False, snapshot_configs=None):
@@ -16,3 +18,37 @@ def redis_server_resource(purpose, host, port, persisted_by_aof=False, snapshot_
         directory_resource(path=data_directory, owner=CURRENT_USER, group=CURRENT_USER_GROUP, mode=0770),
         file_resource(path=VEIL_ETC_DIR / '{}-redis.conf'.format(purpose.replace('_', '-')), content=render_config('redis-server.conf.j2', config=redis_config))
     ]
+
+
+@composite_installer
+def redis_server_source_code_resource(purpose, host, port, persisted_by_aof=False, snapshot_configs=None):
+    data_directory = VEIL_DATA_DIR / '{}-redis'.format(purpose.replace('_', '-'))
+    redis_config = DictObject({'host': host, 'port': port, 'data_directory': data_directory, 'persisted_by_aof': persisted_by_aof,
+                               'snapshot_configs': snapshot_configs or ()})
+    if redis_config.snapshot_configs:
+        redis_config.dump_file = '{}-backup.rdb'.format(purpose)
+
+    return [
+        redis_server_os_source_code_resource(version=REDIS_SERVER_SOURCE_CODE_VERSION),
+        directory_resource(path=data_directory, owner=CURRENT_USER, group=CURRENT_USER_GROUP, mode=0770),
+        file_resource(path=VEIL_ETC_DIR / '{}-redis.conf'.format(purpose.replace('_', '-')), content=render_config('redis-server.conf.j2', config=redis_config))
+    ]
+
+
+@atomic_installer
+def redis_server_os_source_code_resource(version):
+    installed_file_name = '.installed'
+    tgz_name = 'redis-{}.tar.gz'.format(version)
+    local_path = DEPENDENCY_DIR / tgz_name
+    if not local_path.exists():
+        shell_execute('wget -c http://download.redis.io/releases/{} -O {}'.format(tgz_name, local_path))
+    install_path = DEPENDENCY_INSTALL_DIR / tgz_name
+    if not install_path.exists():
+        shell_execute('tar zxvf {} -C {}'.format(tgz_name, DEPENDENCY_INSTALL_DIR), cwd=DEPENDENCY_DIR)
+    installed_path = install_path / installed_file_name
+    if installed_path.exists():
+        return
+    redis_source_code_path = DEPENDENCY_INSTALL_DIR / tgz_name
+    shell_execute('make', cwd=redis_source_code_path)
+    shell_execute('sudo make install', cwd=redis_source_code_path)
+    shell_execute('touch {}'.format(installed_file_name), cwd=redis_source_code_path)
