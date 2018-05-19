544ad0c4eb4fa8c7b3b53c2b01e1a89cf8a1beb4
TianZhen <tianzhen@honovation.com>
2018-05-19 16:08:45 +0800

    fix: redis source code install installed status

--- src/veil/backend/redis/redis_server_installer.py
+++ src/veil/backend/redis/redis_server_installer.py
@@ -39,7 +39,7 @@ def redis_server_source_code_resource(purpose, host, port, persisted_by_aof=Fals
 
 @atomic_installer
 def redis_server_os_source_code_resource(version):
-    installed_file_name = '.installed'
+    target_file_path = as_path('/usr/local/bin/redis-server')
     tgz_name = 'redis-{}.tar.gz'.format(version)
     local_path = DEPENDENCY_DIR / tgz_name
     if not local_path.exists():
@@ -47,9 +47,6 @@ def redis_server_os_source_code_resource(version):
     redis_source_code_path = DEPENDENCY_INSTALL_DIR / 'redis-{}'.format(version)
     if not redis_source_code_path.exists():
         shell_execute('tar zxvf {} -C {}'.format(tgz_name, DEPENDENCY_INSTALL_DIR), cwd=DEPENDENCY_DIR)
-    installed_path = redis_source_code_path / installed_file_name
-    if installed_path.exists():
+    if target_file_path.exists():
         return
-    shell_execute('make', cwd=redis_source_code_path)
-    shell_execute('sudo make install', cwd=redis_source_code_path)
-    shell_execute('touch {}'.format(installed_file_name), cwd=redis_source_code_path)
+    shell_execute('make clean && make && sudo make install', cwd=redis_source_code_path)
