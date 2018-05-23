702ccd403a99ae5ace602967466970e515f5cdd3
TianZhen <loooseleaves@gmail.com>
2018-05-23 13:05:43 +0800

    fix: remove redis source code resource

--- src/veil/backend/redis/__init__.py
+++ src/veil/backend/redis/__init__.py
@@ -3,13 +3,9 @@ with veil_component.init_component(__name__):
     from .redis_client import register_redis
     from .redis_client_installer import redis_client_resource
     from .redis_server_installer import redis_server_resource
-    from .redis_server_installer import redis_server_source_code_resource
-    from .redis_server_installer import redis_server_os_source_code_resource
 
     __all__ = [
         register_redis.__name__,
         redis_client_resource.__name__,
         redis_server_resource.__name__,
-        redis_server_source_code_resource.__name__,
-        redis_server_os_source_code_resource.__name__,
     ]
