0ec1390bf5082fa3e1d6348a163ce46dadf4e7a1
TianZhen <loooseleaves@gmail.com>
2018-05-30 14:24:57 +0800

    fix: image aliases

--- src/veil/environment/in_service/host_installer.py
+++ src/veil/environment/in_service/host_installer.py
@@ -99,7 +99,7 @@ def veil_host_lxd_image_resource(host):
     client = LXDClient(endpoint=host.lxd_endpoint, config_dir=host.env_config_dir).client
     if client.images.exists(LXD_IMAGE_FINGERPRINT):
         image = client.images.get(LXD_IMAGE_FINGERPRINT)
-        if LXD_IMAGE_ALIAS not in image.aliases:
+        if LXD_IMAGE_ALIAS not in [a['name'] for a in image.aliases]:
             image.add_alias(LXD_IMAGE_ALIAS, 'default image')
     else:
         fabric.api.run('lxc image copy {} local: --alias u1804'.format(LXD_IMAGE_FINGERPRINT))
