bcfe5199e78a330e0803a783bf4300f991c78e0f
Eric Du <eric@honovation.com>
2018-06-04 13:22:28 +0800

    enable PG checksums

--- src/veil/backend/database/postgresql/server/pg_server_installer.py
+++ src/veil/backend/database/postgresql/server/pg_server_installer.py
@@ -195,6 +195,7 @@ def postgresql_cluster_resource(purpose, version, owner, owner_password):
     install_resource(file_resource(path='/tmp/pg-{}-owner-password'.format(purpose), content=owner_password, owner=CURRENT_USER, group=CURRENT_USER_GROUP))
     initdb_command = (
         '{pg_bin_dir}/initdb '
+        '--data-checksums '
         '-E UTF-8 '
         '--locale=zh_CN.UTF-8 '
         '--lc-collate=C '
