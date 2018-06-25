8f1c1f63423484acbb00392b2011ad490ee14ff4
TianZhen <loooseleaves@gmail.com>
2018-06-25 17:08:28 +0800

    fix: compat

--- src/veil/backend/redis_setting.py
+++ src/veil/backend/redis_setting.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals, print_function, division
 from veil.profile.installer import *
 
 
-def redis_program(purpose, host, port, persisted_by_aof=False, snapshot_configs=None):
+def redis_program(purpose, host, port, max_memory=None, max_memory_policy=None, enable_aof=False, aof_fsync=None, enable_snapshot=True, snapshot_configs=()):
     return objectify({
         '{}_redis'.format(purpose): {
             'execute_command': 'redis-server {}'.format(VEIL_ETC_DIR / '{}-redis.conf'.format(purpose.replace('_', '-'))),
@@ -12,7 +12,11 @@ def redis_program(purpose, host, port, persisted_by_aof=False, snapshot_configs=
                 'purpose': purpose,
                 'host': host,
                 'port': port,
-                'persisted_by_aof': persisted_by_aof,
+                'max_memory': max_memory,
+                'max_memory_policy': max_memory_policy,
+                'enable_aof': enable_aof,
+                'aof_fsync': aof_fsync,
+                'enable_snapshot': enable_snapshot,
                 'snapshot_configs': snapshot_configs
             })]
         }
