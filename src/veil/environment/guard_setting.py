beb94488a72d4d3487a32e82367575573b4e79c6
TianZhen <loooseleaves@gmail.com>
2018-06-25 17:15:20 +0800

    fix: compat

--- src/veil/environment/guard_setting.py
+++ src/veil/environment/guard_setting.py
@@ -2,27 +2,23 @@ from __future__ import unicode_literals, print_function, division
 from veil.profile.installer import *
 
 
-def guard_program(crontab_expression, redis_aof_shipping_config=None, bucket_shipping_config=None):
-    program_config = objectify({
-        'guard': {
-            'execute_command': "veil environment backup guard-up '{}'".format(crontab_expression),
-            'run_as': 'root',
-            'resources': [('veil_installer.component_resource', {
-                'name': 'veil.environment.backup'
-            })]
+def guard_program(host_backup_crontab_expression='7 4 * * *', bucket_sync_config=None, redis_sync_config=None):
+    program_config = DictObject()
+    if host_backup_crontab_expression:
+        program_config['host_backup'] = {
+            'execute_command': "veil environment backup host-backup '{}'".format(host_backup_crontab_expression),
+            'resources': [('veil_installer.component_resource', {'name': 'veil.environment.backup'})]
         }
-    })
-    if redis_aof_shipping_config:
-        program_config['redis_snapshot_shipping'] = {
-            'execute_command': "veil environment backup aof-shipping {} {} '{}'".format(','.join(redis_aof_shipping_config.purposes),
-                                                                                        redis_aof_shipping_config.remote_path,
-                                                                                        redis_aof_shipping_config.crontab_expression),
-            'run_as': 'root'
+    if bucket_sync_config:
+        program_config['bucket_sync'] = {
+            'execute_command': 'veil environment backup bucket-sync {}'.format(','.join(bucket_sync_config.exclude_buckets)),
+            'resources': [('veil_installer.component_resource', {'name': 'veil.environment.backup'})]
         }
-    if bucket_shipping_config:
-        program_config['bucket_shipping'] = {
-            'execute_command': 'veil environment backup bucket-shipping {} {}'.format(','.join(bucket_shipping_config.exclude_buckets),
-                                                                                      bucket_shipping_config.remote_path),
-            'run_as': 'root'
+    if redis_sync_config:
+        if not hasattr(redis_sync_config, 'crontab_expression'):
+            redis_sync_config.crontab_expression = '*/12 * * * *'
+        program_config['redis_sync'] = {
+            'execute_command': "veil environment backup redis-sync {} '{}'".format(','.join(redis_sync_config.purposes), redis_sync_config.crontab_expression),
+            'resources': [('veil_installer.component_resource', {'name': 'veil.environment.backup'})]
         }
     return program_config
