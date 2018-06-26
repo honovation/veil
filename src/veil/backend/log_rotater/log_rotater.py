4a1507bef6ba2823b59c0de33e0debe3f1bceea6
Eric Du <eric@honovation.com>
2018-06-26 16:49:32 +0800

    run log rotater as current user, instead of root

--- src/veil/backend/log_rotater/log_rotater.py
+++ src/veil/backend/log_rotater/log_rotater.py
@@ -1,16 +1,18 @@
 from __future__ import unicode_literals, print_function, division
 import logging
+
+from veil.environment import VEIL_LOG_DIR
 from veil.frontend.cli import *
 from veil.utility.timer import *
 from veil.utility.shell import *
 
-
 LOGGER = logging.getLogger(__name__)
+
 
 @script('up')
 def bring_up_log_rotater(config_file, crontab_expression):
     @run_every(crontab_expression)
     def work():
-        shell_execute('logrotate {}'.format(config_file))
+        shell_execute('logrotate -s {}/logrotate-status {}'.format(VEIL_LOG_DIR, config_file))
 
     work()
