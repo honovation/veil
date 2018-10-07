45bdd43ef66e9eda8321fa6ac464ea76a170873e
Eric Du <eric@honovation.com>
2018-10-08 07:12:02 +0800

    ChromeDriver only needed under test env.

--- src/veil/development/selenium/selenium_installer.py
+++ src/veil/development/selenium/selenium_installer.py
@@ -1,9 +1,13 @@
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals, print_function, division
+
 from veil_installer import *
+from veil.server.os import *
 from veil.server.python import *
 
 
 @atomic_installer
 def selenium_python_package_resource():
+    # check https://github.com/SeleniumHQ/selenium/wiki/ChromeDriver
+    install_resource(os_package_resource(name='chromium-chromedriver'))
     install_resource(python_package_resource(name='selenium'))
